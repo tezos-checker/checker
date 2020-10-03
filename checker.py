6db1edc6413a952b1c3f6759504bd1daa63e8fcd
Arthur Breitman <murbard@users.noreply.github.com>
2020-10-03 16:14:14 +0100

    Update checker.py
    make it explicit that log doesn't need to be computed, fix computation of min_value and max_value

--- checker.py
+++ checker.py
@@ -11,29 +11,28 @@ def next_quantity(q, drift, next_drift, rate_of_change_drift, last_t, current_t)
     time_difference = current_t - last_t
     return q * math.exp(
         (drift + (
-            (1/6) * (2 * drift + next_drift) * time_difference)) *
+            (1.0 / 6.0) * (2.0 * drift + next_drift) * time_difference)) *
         time_difference)
 
 def rate_of_change_of_drift(p):
-    def sign(val):
-        if val < 0:
-            return -1
-        if val > 0:
-            return 1
-        return 0
-    log_p = math.log(p)
-    if log_p < 0.005:
-        return 0
-    if log_p < 0.05:
-        return sign(log_p) * 0.01
-    return sign(log_p) * 0.05
+    def sign_of_log(val):
+        if val < 1.0:
+            return -1.0
+        if val > 1.0:
+            return 1.0
+        return 0.0
+    if p < math.exp(0.005):
+        return 0.0
+    if p < math.exp(0.05):
+        return sign_of_log(p) * 0.01
+    return sign_of_log(p) * 0.05
 
 def target_price_in_tez(q, tz, k):
-    p = (q * tz)/k
+    p = (q * tz) / k
     return p
 
 def protected_index(last_protected_index, tz, interval):
     epsilon = 0.0005 # 0.05 cNp/min
-    min_value = math.pow(-math.e, epsilon * interval)
-    max_value = math.pow(math.e, epsilon * interval)
+    min_value = math.exp(- epsilon * interval)
+    max_value = math.exp( epsilon * interval)
     return last_protected_index * max(min(tz/last_protected_index, max_value), min_value)
