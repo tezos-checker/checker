1cef080867b77de6a6cce086ff1dc8e464826544
Utku Demir <me@utdemir.com>
2020-10-07 16:56:58 +1300

    Add Nix expressions with ocaml

---
+++ shell.nix
@@ -0,0 +1,13 @@
+let
+sources = import ./nix/sources.nix {};
+pkgs = import sources.nixpkgs {};
+ocamlPackages = pkgs.ocamlPackages;
+in
+pkgs.mkShell {
+  name = "huxian";
+  buildInputs = [
+    ocamlPackages.ocaml
+    ocamlPackages.ocp-indent
+    ocamlPackages.merlin
+  ];
+}
