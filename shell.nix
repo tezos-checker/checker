feb47865f12acb13383df8fd8aa6e14c986db59d
Steve Purcell <steve@sanityinc.com>
2020-10-19 16:18:07 +1300

    Add ounit package

--- shell.nix
+++ shell.nix
@@ -1,14 +1,16 @@
 let
-sources = import ./nix/sources.nix {};
-pkgs = import sources.nixpkgs {};
-ocamlPackages = pkgs.ocamlPackages;
+  sources = import ./nix/sources.nix { };
+  pkgs = import sources.nixpkgs { };
+  ocamlPackages = pkgs.ocamlPackages;
 in
 pkgs.mkShell {
   name = "huxian";
   buildInputs = [
     ocamlPackages.ocaml
+    ocamlPackages.findlib # Lets merlin see packages like ounit
     ocamlPackages.ocp-indent
     ocamlPackages.merlin
     ocamlPackages.ocamlbuild
+    ocamlPackages.ounit
   ];
 }
