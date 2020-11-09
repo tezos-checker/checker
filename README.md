fb3b877b73d52b97882fcb93ceb590f66716c81a
Steve Purcell <steve@sanityinc.com>
2020-11-09 19:21:39 +1300

    Move liquidation experiment code into a unit test

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 * Technical document can be found [here](https://hackmd.io/teMO2x9PRRy1iTBtrSMBvA?view).
 * The contents of the interface file in the technical document have been slightly adjusted (so that compilation succeeds) and are placed in the [src folder](./src).
-* Currently all the action is happening on the [monolithic/script.ml](./monolithic/script.ml) file.
+* Currently all the action is happening in code in the [monolithic](./monolithic) folder.
 * The initial python sketches are in the [python folder](./python).
--- monolithic/testLiquidation.ml
+++ monolithic/testLiquidation.ml
@@ -1,3 +1,11 @@
+open Parameters
+open FixedPoint
+open Tez
+open Kit
+open Timestamp
+open Burrow
+open Address
+open Liquidation
 open OUnit2
 
 (*
@@ -25,7 +33,68 @@ If a liquidation was deemed Close:
 * the resulting burrow is zeroed and inactive
 *)
 
+let tez_of_string s = Tez.scale Tez.one (FixedPoint.of_string s)
+
+let params : Parameters.t =
+  { q = FixedPoint.of_string "1.015";
+    index = Tez.of_mutez 320_000;
+    protected_index = Tez.of_mutez 360_000;
+    target = FixedPoint.of_string "1.08";
+    drift = FixedPoint.of_string "0.0";
+    drift' = FixedPoint.of_string "0.0";
+    burrow_fee_index = FixedPoint.of_string "1.0";
+    imbalance_index = FixedPoint.of_string "1.0";
+    outstanding_kit = Kit.one; (* TODO: What should that be? *)
+    circulating_kit = Kit.zero; (* TODO: What should that be? *)
+    last_touched = Timestamp.of_seconds 1;
+  }
+
+let initial_burrow : Burrow.t =
+  { owner = Address.of_string "192837";
+    delegate = None;
+    active = true;
+    collateral = Tez.of_mutez 10_000_000;
+    outstanding_kit = Kit.of_string "20.0";
+    adjustment_index = Parameters.compute_adjustment_index params;
+    collateral_at_auction = Tez.zero;
+    last_touched = Timestamp.of_seconds 0;
+  }
+
 let suite =
   "LiquidationTests" >::: [
-    (* TODO: Add tests here *)
+    "liquidation test" >:: fun _ ->
+      (* OTHER EXAMPLES *)
+      (* Unwarranted liquidation for *)
+      (* let initial_burrow = { minted_kit = Kit.of_float 10.0; collateral = Tez.of_float 10.0; } in *)
+      (* Partial liquidation for *)
+      (* let initial_burrow = { minted_kit = Kit.of_float 20.0; collateral = Tez.of_float 10.0; } in *)
+      (* Complete liquidation (deplete the collateral, but keep the burrow) for *)
+      (* let initial_burrow = { minted_kit = Kit.of_float 100.0; collateral = Tez.of_float 10.0; } in *)
+      (* Complete liquidation (close the burrow) for *)
+      (* let initial_burrow = { minted_kit = Kit.of_float 100.0; collateral = Tez.of_float 1.001; } in *)
+      (* DEFAULT *)
+      let burrow = initial_burrow in
+
+      assert_bool "is overburrowed" (Burrow.is_overburrowed params burrow);
+      assert_bool "is optimistically overburrowed" (Burrow.is_optimistically_overburrowed params burrow);
+      assert_bool "is liquidatable" (Burrow.is_liquidatable params burrow);
+
+      let liquidation_result = request_liquidation params burrow in
+
+      assert_equal Partial liquidation_result.outcome ~printer:Liquidation.show_liquidation_outcome;
+
+      let new_burrow = liquidation_result.burrow_state in
+      assert_equal { burrow with
+                     collateral = Tez.of_mutez 2_633_201;
+                     outstanding_kit = Kit.of_string "20";
+                     collateral_at_auction = Tez.of_mutez 6_356_799; }
+        new_burrow ~printer:Burrow.show;
+      assert_equal
+        Tez.(new_burrow.collateral + new_burrow.collateral_at_auction + liquidation_result.liquidation_reward)
+        Tez.(burrow.collateral + burrow.collateral_at_auction)
+        ~printer:Tez.show;
+      assert_bool "not now optimistically overburrowed" (not (Burrow.is_optimistically_overburrowed params new_burrow));
+      assert_bool "not now liquidatable" (not (Burrow.is_liquidatable params new_burrow));
+      (* FIXME: this seems broken!?  *)
+      assert_bool "still overburrowed" (Burrow.is_overburrowed params new_burrow);
   ]
