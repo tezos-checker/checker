001d19581e2e3d9129cf7acc2990f598ed3e149a
Steve Purcell <steve@sanityinc.com>
2020-11-16 19:49:34 +1300

    Minor fixes in auction tests

--- monolithic/testAuction.ml
+++ monolithic/testAuction.ml
@@ -1,7 +1,5 @@
 open OUnit2
 
-type some_kit = Kit.t option [@@deriving show]
-
 
 let suite =
   let burrow_id_1 = Ptr.init in
@@ -22,14 +20,15 @@ let suite =
        let start_time = (Timestamp.of_seconds 0) in
        let start_price = Kit.one in
        let auctions = Auction.touch auctions start_time start_price in
+       let current = Option.get auctions.current_auction in
        assert_equal (Some (Tez.of_mutez 2_000_000)) (Auction.current_auction_tez auctions);
-       assert_equal (Some (Kit.of_mukit 2_000_000)) (Option.map (Auction.current_auction_minimum_bid start_time) auctions.current_auction) ~printer:show_some_kit;
-       assert_equal (Some (Kit.of_mukit 2_000_000)) (Option.map (Auction.current_auction_minimum_bid start_time) auctions.current_auction) ~printer:show_some_kit;
+       assert_equal (Kit.of_mukit 2_000_000) (Auction.current_auction_minimum_bid start_time current) ~printer:Kit.show;
+       assert_equal (Kit.of_mukit 2_000_000) (Auction.current_auction_minimum_bid start_time current) ~printer:Kit.show;
        (* Price of descending auction should go down... *)
        let one_second_later = Timestamp.add_seconds start_time 1 in
-       assert_equal (Some (Kit.of_mukit 1_998_000)) (Option.map (Auction.current_auction_minimum_bid one_second_later) auctions.current_auction) ~printer:show_some_kit;
+       assert_equal (Kit.of_mukit 1_998_000) (Auction.current_auction_minimum_bid one_second_later current) ~printer:Kit.show;
        let two_seconds_later = Timestamp.add_seconds start_time 2 in
-       assert_equal (Some (Kit.of_mukit 1_996_002)) (Option.map (Auction.current_auction_minimum_bid two_seconds_later) auctions.current_auction) ~printer:show_some_kit;
+       assert_equal (Kit.of_mukit 1_996_002) (Auction.current_auction_minimum_bid two_seconds_later current) ~printer:Kit.show;
     );
 
     ("test batches up auction lots" >::
@@ -101,14 +100,14 @@ let suite =
        let bidder = Address.of_string "23456" in
        let current = Option.get auctions.current_auction in
 
-       (* Below threshold *)
+       (* Below minimum bid *)
        assert_equal (Error Auction.BidTooLow) (Auction.place_bid start_time current { address = bidder; kit = Kit.of_mukit 1_000_000; });
-       (* Right below threshold *)
+       (* Right below minimum bid *)
        assert_equal (Error Auction.BidTooLow) (Auction.place_bid start_time current { address = bidder; kit = Kit.of_mukit 1_999_999; });
-       (* On/Above threshold, we get a bid ticket and our bid plus 0.33 cNp becomes the new threshold *)
+       (* On/Above minimum bid, we get a bid ticket and our bid plus 0.33 cNp becomes the new minimum bid *)
        let (current, _ticket) = Result.get_ok (Auction.place_bid start_time current { address = bidder; kit = Kit.of_mukit 2_000_000; }) in
        assert_equal (Kit.of_mukit 2_006_600) (Auction.current_auction_minimum_bid start_time current) ~printer:Kit.show;
-       (* Threshold does not drop over time *)
+       (* Minimum bid does not drop over time *)
        assert_equal (Kit.of_mukit 2_006_600) (Auction.current_auction_minimum_bid (Timestamp.add_seconds start_time 10) current) ~printer:Kit.show;
-    )
+    );
   ]
