7da6182b91ad236633812aa0065757e8e1c8d773
Georgios Karachalias <george.karachalias@gmail.com>
2021-01-14 14:50:18 +0100

    Check the validity of delegation auction tickets

--- src/delegationAuction.ml
+++ src/delegationAuction.ml
@@ -3,12 +3,38 @@ type bid = { bidder: Address.t; cycle: int; amount: Tez.t }
 
 type bid_ticket = bid Ticket.t
 
+let issue_bid_ticket (tezos: Tezos.t) (bid: bid) =
+  Ticket.create ~issuer:tezos.self ~amount:Nat.one ~content:bid
+
 type Error.error +=
   | BidTooLow
   | BidTicketExpired
   | CannotReclaimLeadingBid
   | CannotReclaimWinningBid
   | NotAWinningBid
+  | InvalidDelegationAuctionTicket
+
+(** Check whether a delegation auction bid ticket is valid. A delegation bid
+  * ticket is valid if (a) it is issued by checker, (b) its amount is exactly 1
+  * (avoids splitting it), and (c) is tagged appropriately. TODO: (c) is not
+  * implemented yet. Perhaps it can be avoided, if all checker-issued tickets
+  * end up having contents clearly distinguished by type. *)
+let is_bid_ticket_valid
+    ~(tezos:Tezos.t)
+    ~(bid_ticket: bid_ticket)
+  : (bid_ticket, Error.error) result =
+  let issuer, amount, _bid_details, same_ticket = Ticket.read bid_ticket in
+  let is_valid = issuer = tezos.self && amount = Nat.one in
+  if is_valid then Ok same_ticket else Error InvalidDelegationAuctionTicket
+
+let with_valid_bid_ticket
+    ~(tezos:Tezos.t)
+    ~(bid_ticket: bid_ticket)
+    (f: bid_ticket -> ('a, Error.error) result)
+  : ('a, Error.error) result =
+  match is_bid_ticket_valid ~tezos ~bid_ticket with
+  | Error err -> Error err
+  | Ok ticket -> f ticket
 
 type t = { cycle: int; winner: bid option; leading_bid: bid option; delegate: Address.t option; }
 [@@deriving show]
@@ -42,12 +68,13 @@ let place_bid t (tezos: Tezos.t) ~sender ~amount =
   | _ ->
     (* Either there is no bid or this is the highest *)
     let bid = {bidder=sender; cycle=t.cycle; amount=amount} in
-    let ticket = Ticket.create ~issuer:tezos.self ~amount:Nat.one ~content:bid in
+    let ticket = issue_bid_ticket tezos bid in
     Ok (ticket, {t with leading_bid = Some bid;})
 
 (* TODO: allow winner to nominate a different address as the delegate? *)
 let claim_win t tezos ~bid_ticket =
   let t = touch t tezos in
+  with_valid_bid_ticket ~tezos ~bid_ticket @@ fun bid_ticket ->
   let (_, _, bid, _) = Ticket.read bid_ticket in
   if Some bid = t.winner then
     Ok { t with delegate = Some bid.bidder }
@@ -57,6 +84,7 @@ let claim_win t tezos ~bid_ticket =
 (* TODO use address *)
 let reclaim_bid t tezos ~address:_ ~bid_ticket =
   let t = touch t tezos in
+  with_valid_bid_ticket ~tezos ~bid_ticket @@ fun bid_ticket ->
   let (_, _, bid, _) = Ticket.read bid_ticket in
   if Some bid = t.leading_bid then
     Error CannotReclaimLeadingBid
@@ -66,6 +94,3 @@ let reclaim_bid t tezos ~address:_ ~bid_ticket =
     Error BidTicketExpired
   else
     Ok (bid.amount, t)
-
-(* TODO check ticket amount *)
-(* TODO check ticket issuer *)
