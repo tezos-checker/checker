7a75449f57705eb11afaf6d5a3e6588646365992
Steve Purcell <steve@sanityinc.com>
2021-01-26 16:10:06 +1300

    Test claiming delegation win too early / late

--- src/testChecker.ml
+++ src/testChecker.ml
@@ -184,12 +184,39 @@ let suite =
          ~printer:Checker.show_tez_payment;
     );
 
+    ("Can't claim delegation too soon after winning delegation auction" >::
+     fun _ ->
+       Ligo.Tezos.reset ();
+       let checker = Checker.initial_checker in
+       Ligo.Tezos.new_transaction ~seconds_passed:0 ~blocks_passed:0 ~sender:alice_addr ~amount:(Ligo.tez_from_literal "1_000_000mutez");
+       let ticket, checker = Checker.delegation_auction_place_bid checker in
+
+       assert_raises (Failure "NotAWinningBid") (fun _ ->
+           Ligo.Tezos.new_transaction ~seconds_passed:(60 * 4095) ~blocks_passed:4095 ~sender:alice_addr ~amount:(Ligo.tez_from_literal "0mutez");
+           let _checker = Checker.delegation_auction_claim_win checker ticket in
+           ());
+    );
+
+    ("Can't claim delegation too late after winning delegation auction" >::
+     fun _ ->
+       Ligo.Tezos.reset ();
+       let checker = Checker.initial_checker in
+       Ligo.Tezos.new_transaction ~seconds_passed:0 ~blocks_passed:0 ~sender:alice_addr ~amount:(Ligo.tez_from_literal "1_000_000mutez");
+       let ticket, checker = Checker.delegation_auction_place_bid checker in
+
+       assert_raises (Failure "NotAWinningBid") (fun _ ->
+           Ligo.Tezos.new_transaction ~seconds_passed:(60 * 9000) ~blocks_passed:9000 ~sender:alice_addr ~amount:(Ligo.tez_from_literal "0mutez");
+           let _checker = Checker.delegation_auction_claim_win checker ticket in
+           ());
+    );
+
     ("Can claim delegation after winning delegation auction" >::
      fun _ ->
        Ligo.Tezos.reset ();
        let checker = Checker.initial_checker in
        Ligo.Tezos.new_transaction ~seconds_passed:0 ~blocks_passed:0 ~sender:alice_addr ~amount:(Ligo.tez_from_literal "1_000_000mutez");
        let ticket, checker = Checker.delegation_auction_place_bid checker in
+
        Ligo.Tezos.new_transaction ~seconds_passed:(60 * 4096) ~blocks_passed:4096 ~sender:alice_addr ~amount:(Ligo.tez_from_literal "0mutez");
        let _checker = Checker.delegation_auction_claim_win checker ticket in
        ();
