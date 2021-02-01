85e6c5b44aef418c25f21040c76c3467d89e63af
Georgios Karachalias <george.karachalias@gmail.com>
2021-02-01 16:39:19 +0100

    Revert the entrypoints' commits

--- src/checker.ml
+++ src/checker.ml
@@ -299,23 +299,18 @@ let set_burrow_delegate (state: t) (permission: permission) (burrow_id: burrow_i
   else
     (failwith "InsufficientPermission": t)
 
-let make_permission (state: t) (permission: permission) (burrow_id: burrow_id) (right: rights) : (LigoOp.operation list * t) =
+let make_permission (state: t) (permission: permission) (burrow_id: burrow_id) (right: rights) : permission =
   assert_no_tez_given ();
   let burrow = find_burrow state burrow_id in
   assert_burrow_has_no_unclaimed_slices state burrow;
   let r = assert_valid_permission permission burrow_id burrow in
   if is_admin_right r then
     (* only admins can create permissions. *)
-    let ticket =
-      Ligo.Tezos.create_ticket
-        (right, burrow_id, burrow_permission_version burrow)
-        (Ligo.nat_from_literal "0n") in
-    let op = match (LigoOp.Tezos.get_entrypoint_opt "%transfer_permission" !Ligo.Tezos.sender : permission LigoOp.contract option) with
-      | Some c -> LigoOp.Tezos.perm_transaction ticket (Ligo.tez_from_literal "0mutez") c
-      | None -> (failwith "unsupported operation, looks like" : LigoOp.operation) in
-    ([op], state) (* unchanged state *)
+    Ligo.Tezos.create_ticket
+      (right, burrow_id, Ligo.nat_from_literal "0n")
+      (Ligo.nat_from_literal "0n")
   else
-    (failwith "InsufficientPermission": LigoOp.operation list * t)
+    (failwith "InsufficientPermission": permission)
 
 let invalidate_all_permissions (state: t) (permission: permission) (burrow_id: burrow_id) : (LigoOp.operation list * t) =
   assert_no_tez_given ();
@@ -723,15 +718,11 @@ let liquidation_auction_place_bid (state: t) (kit: kit_token) : LigoOp.operation
     }
   )
 
-let liquidation_auction_reclaim_bid (state: t) (bid_ticket: liquidation_auction_bid_ticket) : (LigoOp.operation list * t) =
+let liquidation_auction_reclaim_bid (state: t) (bid_ticket: liquidation_auction_bid_ticket) : kit_token =
   assert_no_tez_given ();
   let bid_ticket = liquidation_auction_assert_valid_bid_ticket bid_ticket in
   let kit = liquidation_auction_reclaim_bid state.liquidation_auctions bid_ticket in
-  let kit_tokens = kit_issue kit in (* TODO: should not issue; should change the auction logic instead! *)
-  let op = match (LigoOp.Tezos.get_entrypoint_opt "%transfer_kit" !Ligo.Tezos.sender : kit_token LigoOp.contract option) with
-    | Some c -> LigoOp.Tezos.kit_transaction kit_tokens (Ligo.tez_from_literal "0mutez") c
-    | None -> (failwith "unsupported operation, looks like" : LigoOp.operation) in
-  ([op], state) (* NOTE: unchanged state. It's a little weird that we don't keep track of how much kit has not been reclaimed. *)
+  kit_issue kit (* TODO: should not issue; should change the auction logic instead! *)
 
 let liquidation_auction_reclaim_winning_bid (state: t) (bid_ticket: liquidation_auction_bid_ticket) : (LigoOp.operation list * t) =
   assert_no_tez_given ();
@@ -848,103 +839,13 @@ let touch (state: t) (index:Ligo.tez) : (LigoOp.operation list * t) =
 
 type params =
   | Touch
-  (* Burrows *)
-  | CreateBurrow
-  | DepositTez of (permission option * burrow_id)
-  | WithdrawTez of (permission * Ligo.tez * burrow_id)
-  | MintKit of (permission * burrow_id * kit)
-  | BurnKit of (permission option * burrow_id * kit_token)
-  | ActivateBurrow of (permission * burrow_id)
-  | DeactivateBurrow of (permission * burrow_id * Ligo.address)
-  | MarkBurrowForLiquidation of burrow_id
-  | TouchLiquidationSlices of leaf_ptr list
-  | CancelSliceLiquidation of (permission * leaf_ptr)
-  | TouchBurrow of burrow_id
-  | SetBurrowDelegate of (permission * burrow_id * Ligo.address)
-  | MakePermission of (permission * burrow_id * rights)
-  | InvalidateAllPermissions of (permission * burrow_id)
-  (* Uniswap *)
-  | BuyKit of (kit * Ligo.timestamp)
-  | SellKit of (kit_token * Ligo.tez * Ligo.timestamp)
-  | AddLiquidity of (kit_token * Ligo.nat * Ligo.timestamp)
-  | RemoveLiquidity of (liquidity * Ligo.tez * kit * Ligo.timestamp)
-  (* Liquidation Auction *)
-  | LiqAuctionPlaceBid of kit_token
-  | LiqAuctionReclaimBid of liquidation_auction_bid_ticket
-  | LiqAuctionReclaimWinningBid of liquidation_auction_bid_ticket
-  (* Delegation Auction *)
-  | DelegationAuctionPlaceBid
   | DelegationAuctionClaimWin of (delegation_auction_bid Ligo.ticket * Ligo.key_hash)
-  | DelegationAuctionReclaimBid of delegation_auction_bid Ligo.ticket
 
 let main (op, state: params * t): LigoOp.operation list * t =
   match op with
-  | Touch ->
-    touch state (Ligo.tez_from_literal "0mutez") (* FIXME: medianizer input?? *)
-  (* Burrows *)
-  | CreateBurrow ->
-    (failwith "FIXME" : LigoOp.operation list * t) (* FIXME: origination operation *)
-  | DepositTez p ->
-    let (permission_option, burrow_id) = p in
-    deposit_tez state permission_option burrow_id
-  | WithdrawTez p ->
-    let (permission, tez, burrow_id) = p in
-    withdraw_tez state permission tez burrow_id
-  | MintKit p ->
-    let (permission, burrow_id, kit) = p in
-    mint_kit state permission burrow_id kit
-  | BurnKit p ->
-    let (permission_option, burrow_id, kit_token) = p in
-    burn_kit state permission_option burrow_id kit_token
-  | ActivateBurrow p ->
-    let (permission, burrow_id) = p in
-    activate_burrow state permission burrow_id
-  | DeactivateBurrow p ->
-    let (permission, burrow_id, addr) = p in
-    deactivate_burrow state permission burrow_id addr
-  | MarkBurrowForLiquidation burrow_id ->
-    mark_for_liquidation state burrow_id
-  | TouchLiquidationSlices slices ->
-    touch_liquidation_slices (state, slices)
-  | CancelSliceLiquidation p ->
-    let (permission, leaf_ptr) = p in
-    cancel_liquidation_slice state permission leaf_ptr
-  | TouchBurrow burrow_id ->
-    touch_burrow state burrow_id
-  | SetBurrowDelegate p ->
-    let (_permission, _burrow_id, _addr) = p in
-    (failwith "FIXME" : LigoOp.operation list * t) (* FIXME: burrow contract entrypoints *)
-  | MakePermission p ->
-    let (permission, burrow_id, rights) = p in
-    make_permission state permission burrow_id rights
-  | InvalidateAllPermissions p ->
-    let (permission, burrow_id) = p in
-    invalidate_all_permissions state permission burrow_id
-  (* Uniswap *)
-  | BuyKit p ->
-    let (min_kit, deadline) = p in
-    buy_kit state min_kit deadline
-  | SellKit p ->
-    let (kit_token, min_tez, deadline) = p in
-    sell_kit state kit_token min_tez deadline
-  | AddLiquidity p ->
-    let (max_kit_token, min_liquidity, deadline) = p in
-    add_liquidity state max_kit_token min_liquidity deadline
-  | RemoveLiquidity p ->
-    let (liquidity, min_tez, min_kit, deadline) = p in
-    remove_liquidity state liquidity min_tez min_kit deadline
-  (* Liquidation Auction *)
-  | LiqAuctionPlaceBid kit_token ->
-    liquidation_auction_place_bid state kit_token
-  | LiqAuctionReclaimBid ticket ->
-    liquidation_auction_reclaim_bid state ticket
-  | LiqAuctionReclaimWinningBid ticket ->
-    liquidation_auction_reclaim_winning_bid state ticket
-  (* Delegation Auction *)
-  | DelegationAuctionPlaceBid ->
-    delegation_auction_place_bid state
   | DelegationAuctionClaimWin p ->
     let (ticket, key) = p in
     delegation_auction_claim_win state ticket key
-  | DelegationAuctionReclaimBid ticket ->
-    delegation_auction_reclaim_bid state ticket
+  | Touch ->
+    let ops, state = touch state (Ligo.tez_from_literal "0mutez") in
+    (ops, state)
--- src/checker.mli
+++ src/checker.mli
@@ -6,11 +6,10 @@ open Uniswap
 open Burrow
 open DelegationAuction
 open LiquidationAuction
-open LiquidationAuctionTypes
 open TokenTypes
 
 (* TODO: Actually, at the end, this should be a Michelson address, which we
- * receive when we originate the burrow contract (Tezos.create_contract). *)
+ * receive when we originate the burrow contract (Tezos.create_ticket_contract). *)
 type burrow_id = Ptr.t
 
 type t =
@@ -86,7 +85,7 @@ val mark_for_liquidation : t -> burrow_id -> (LigoOp.operation list * t)
 (** Process the liquidation slices on completed liquidation auctions. Invalid
   * leaf_ptr's fail, and slices that correspond to incomplete liquidations are
   * ignored. *)
-val touch_liquidation_slices : t * leaf_ptr list -> (LigoOp.operation list * t)
+val touch_liquidation_slices : t * LiquidationAuctionTypes.leaf_ptr list -> (LigoOp.operation list * t)
 
 (** Cancel the liquidation of a slice. The burden is on the caller to provide
   * both the burrow_id and the leaf_ptr. This operation can fail for several
@@ -97,16 +96,16 @@ val touch_liquidation_slices : t * leaf_ptr list -> (LigoOp.operation list * t)
   * - if the slice is part of an already completed auction,
   * - if the burrow is overburrowed at the moment.
 *)
-val cancel_liquidation_slice : t -> permission -> leaf_ptr -> (LigoOp.operation list * t)
+val cancel_liquidation_slice : t -> permission -> LiquidationAuctionTypes.leaf_ptr -> (LigoOp.operation list * t)
 
 (** Perform maintainance tasks for the burrow. *)
 val touch_burrow : t -> burrow_id -> (LigoOp.operation list * t)
 
 (** Set the delegate of a burrow. *)
-val set_burrow_delegate : t -> permission -> burrow_id -> Ligo.address -> t (* TODO: we need to signal the burrow contract to set its own delegate here! *)
+val set_burrow_delegate : t -> permission -> burrow_id -> Ligo.address -> t (* TODO: yield an (SetDelegate) operation here! *)
 
 (** Requires admin. Create a new permission for a burrow. *)
-val make_permission : t -> permission -> burrow_id -> rights -> (LigoOp.operation list * t)
+val make_permission : t -> permission -> burrow_id -> rights -> permission (* TODO: why not returning the new storage? *)
 
 (** Requires admin. Increments a counter so that all previous permissions are
   * now invalid and returns a new admin permission. This makes it easy to
@@ -146,7 +145,7 @@ val remove_liquidity : t -> liquidity -> Ligo.tez -> kit -> Ligo.timestamp -> (L
 val liquidation_auction_place_bid : t -> kit_token -> (LigoOp.operation list * t)
 
 (** Reclaim a failed bid for the current or a completed liquidation auction. *)
-val liquidation_auction_reclaim_bid : t -> liquidation_auction_bid_ticket -> (LigoOp.operation list * t)
+val liquidation_auction_reclaim_bid : t -> liquidation_auction_bid_ticket -> kit_token (* TODO: why not returning the new storage? *)
 
 (** Reclaim a winning bid for the current or a completed liquidation auction. *)
 val liquidation_auction_reclaim_winning_bid : t -> liquidation_auction_bid_ticket -> (LigoOp.operation list * t)
@@ -174,33 +173,6 @@ val delegation_auction_reclaim_bid : t -> delegation_auction_bid Ligo.ticket -> 
 
 type params =
   | Touch
-  (* Burrows *)
-  | CreateBurrow
-  | DepositTez of (permission option * burrow_id)
-  | WithdrawTez of (permission * Ligo.tez * burrow_id)
-  | MintKit of (permission * burrow_id * kit)
-  | BurnKit of (permission option * burrow_id * kit_token)
-  | ActivateBurrow of (permission * burrow_id)
-  | DeactivateBurrow of (permission * burrow_id * Ligo.address)
-  | MarkBurrowForLiquidation of burrow_id
-  | TouchLiquidationSlices of leaf_ptr list
-  | CancelSliceLiquidation of (permission * leaf_ptr)
-  | TouchBurrow of burrow_id
-  | SetBurrowDelegate of (permission * burrow_id * Ligo.address)
-  | MakePermission of (permission * burrow_id * rights)
-  | InvalidateAllPermissions of (permission * burrow_id)
-  (* Uniswap *)
-  | BuyKit of (kit * Ligo.timestamp)
-  | SellKit of (kit_token * Ligo.tez * Ligo.timestamp)
-  | AddLiquidity of (kit_token * Ligo.nat * Ligo.timestamp)
-  | RemoveLiquidity of (liquidity * Ligo.tez * kit * Ligo.timestamp)
-  (* Liquidation Auction *)
-  | LiqAuctionPlaceBid of kit_token
-  | LiqAuctionReclaimBid of liquidation_auction_bid_ticket
-  | LiqAuctionReclaimWinningBid of liquidation_auction_bid_ticket
-  (* Delegation Auction *)
-  | DelegationAuctionPlaceBid
   | DelegationAuctionClaimWin of (delegation_auction_bid Ligo.ticket * Ligo.key_hash)
-  | DelegationAuctionReclaimBid of delegation_auction_bid Ligo.ticket
 
 val main : params * t -> LigoOp.operation list * t
