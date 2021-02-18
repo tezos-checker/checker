62fc5629d12ad27cb6fce56b7d4f6cdf80379522
Georgios Karachalias <george.karachalias@gmail.com>
2021-02-18 15:03:36 +0100

    Inline operations in permission.ml for tiny effect
    old: 83974 bytes
    new: 83929 bytes
    ======> 45 bytes less

--- src/permission.ml
+++ src/permission.ml
@@ -1,36 +1,36 @@
 open TokenTypes
 
-let does_right_allow_tez_deposits (right: rights) : bool =
+let[@inline] does_right_allow_tez_deposits (right: rights) : bool =
   match right with
   | Admin -> true
   | User r -> r.deposit_tez
 
-let does_right_allow_tez_withdrawals (right: rights) : bool =
+let[@inline] does_right_allow_tez_withdrawals (right: rights) : bool =
   match right with
   | Admin -> true
   | User r -> r.withdraw_tez
 
-let does_right_allow_kit_minting (right: rights) : bool =
+let[@inline] does_right_allow_kit_minting (right: rights) : bool =
   match right with
   | Admin -> true
   | User r -> r.mint_kit
 
-let does_right_allow_kit_burning (right: rights) : bool =
+let[@inline] does_right_allow_kit_burning (right: rights) : bool =
   match right with
   | Admin -> true
   | User r -> r.burn_kit
 
-let does_right_allow_setting_delegate (right: rights) : bool =
+let[@inline] does_right_allow_setting_delegate (right: rights) : bool =
   match right with
   | Admin -> true
   | User r -> r.set_delegate
 
-let does_right_allow_cancelling_liquidations (right: rights) : bool =
+let[@inline] does_right_allow_cancelling_liquidations (right: rights) : bool =
   match right with
   | Admin -> true
   | User r -> r.cancel_liquidation
 
-let is_admin_right (right: rights) : bool =
+let[@inline] is_admin_right (right: rights) : bool =
   match right with
   | Admin -> true
   | User _ -> false
