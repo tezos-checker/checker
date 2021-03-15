633ad1f57f9966fbe660b4f0b0a8fe87a240a8b6
0xd34df00d <0xd34df00d@gmail.com>
2021-03-15 01:12:02 -0500

    Add a script to check if there are duplicate error codes

---
+++ tests/ensure-unique-errors.sh
@@ -0,0 +1,14 @@
+#! env bash
+
+# This awk incantation exists with code 42 if there are duplicates, and 0 otherwise
+grep error_ src/error.ml | awk 'a[$7]++{exit 42}'
+
+ret=$?
+
+if [ $ret == 42 ]; then
+	echo "there are duplicate errors!"
+elif [ $ret == 0 ]; then
+	echo "there are no duplicate error codes!"
+else
+	echo "something else failed"
+fi
