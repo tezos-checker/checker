c8e10f8231e6a1bd839e577545a8ccd25f70b6b3
dorranh <dorran.howell@tweag.io>
2021-03-24 11:24:34 +0100

    Add unit tests for burrow_burn_kit

--- tests/testBurrow.ml
+++ tests/testBurrow.ml
@@ -1,19 +1,140 @@
 open OUnit2
-open TestCommon
-open CheckerTypes
+(* open TestCommon *)
+open Kit
+open FixedPoint
+open Error
 
 let suite =
-  "Checker tests" >::: [
-    (* ("initial touch (noop)" >::
-       fun _ ->
-       Ligo.Tezos.reset ();
-       let checker1 = initial_checker in
-       let ops, checker2 = Checker.touch_with_index checker1 (Ligo.tez_from_literal "0mutez") in
+  "Burrow tests" >::: [
+    ("burrow_burn_kit - fails for a burrow which needs to be touched" >::
+     fun _ ->
+       let burrow0 = Burrow.make_burrow_for_test
+           ~outstanding_kit:(kit_of_mukit (Ligo.nat_from_literal "1n"))
+           ~excess_kit:kit_zero
+           ~active:true
+           ~permission_version:(Ligo.nat_from_literal "0n")
+           ~allow_all_tez_deposits:false
+           ~allow_all_kit_burnings:false
+           ~delegate:None
+           ~collateral:(Ligo.tez_from_literal "0mutez")
+           ~adjustment_index:fixedpoint_one
+           ~collateral_at_auction:(Ligo.tez_from_literal "0mutez")
+           ~liquidation_slices:None
+           ~last_touched:(Ligo.timestamp_from_seconds_literal 0) in
 
-       assert_equal [] ops ~printer:show_operation_list;
-       assert_equal checker1 checker2; (* NOTE: we really want them to be identical here, hence the '='. *)
-       ()
-       ); *)
+       assert_raises
+         (Failure (Ligo.string_of_int error_OperationOnUntouchedBurrow))
+         (fun () ->
+            Burrow.burrow_burn_kit
+              {Parameters.initial_parameters with last_touched=(Ligo.timestamp_from_seconds_literal 1)}
+              (kit_of_mukit (Ligo.nat_from_literal "1n"))
+              burrow0
+         )
+    );
 
+    ("burrow_burn_kit - burning exactly outstanding_kit returns expected burrow state" >::
+     fun _ ->
+       let burrow0 = Burrow.make_burrow_for_test
+           ~outstanding_kit:(kit_of_mukit (Ligo.nat_from_literal "1n"))
+           ~excess_kit:kit_zero
+           ~active:true
+           ~permission_version:(Ligo.nat_from_literal "0n")
+           ~allow_all_tez_deposits:false
+           ~allow_all_kit_burnings:false
+           ~delegate:None
+           ~collateral:(Ligo.tez_from_literal "0mutez")
+           ~adjustment_index:fixedpoint_one
+           ~collateral_at_auction:(Ligo.tez_from_literal "0mutez")
+           ~liquidation_slices:None
+           ~last_touched:(Ligo.timestamp_from_seconds_literal 0) in
+
+       let burrow = Burrow.burrow_burn_kit
+           Parameters.initial_parameters
+           (kit_of_mukit (Ligo.nat_from_literal "1n"))
+           burrow0
+       in
+
+       assert_equal ~printer:show_kit kit_zero (Burrow.burrow_outstanding_kit burrow);
+       assert_equal ~printer:show_kit kit_zero (Burrow.burrow_excess_kit burrow)
+    );
+
+    ("burrow_burn_kit - burning greater than outstanding_kit returns expected burrow state" >::
+     fun _ ->
+       let burrow0 = Burrow.make_burrow_for_test
+           ~outstanding_kit:(kit_of_mukit (Ligo.nat_from_literal "1n"))
+           ~excess_kit:kit_zero
+           ~active:true
+           ~permission_version:(Ligo.nat_from_literal "0n")
+           ~allow_all_tez_deposits:false
+           ~allow_all_kit_burnings:false
+           ~delegate:None
+           ~collateral:(Ligo.tez_from_literal "0mutez")
+           ~adjustment_index:fixedpoint_one
+           ~collateral_at_auction:(Ligo.tez_from_literal "0mutez")
+           ~liquidation_slices:None
+           ~last_touched:(Ligo.timestamp_from_seconds_literal 0) in
+
+       let burrow = Burrow.burrow_burn_kit
+           Parameters.initial_parameters
+           (kit_of_mukit (Ligo.nat_from_literal "2n"))
+           burrow0
+       in
+
+       assert_equal ~printer:show_kit kit_zero (Burrow.burrow_outstanding_kit burrow);
+       assert_equal ~printer:show_kit (kit_of_mukit (Ligo.nat_from_literal "1n")) (Burrow.burrow_excess_kit burrow)
+    );
+
+
+    ("burrow_burn_kit - burning less than outstanding_kit returns expected burrow state" >::
+     fun _ ->
+       let burrow0 = Burrow.make_burrow_for_test
+           ~outstanding_kit:(kit_of_mukit (Ligo.nat_from_literal "2n"))
+           ~excess_kit:kit_zero
+           ~active:true
+           ~permission_version:(Ligo.nat_from_literal "0n")
+           ~allow_all_tez_deposits:false
+           ~allow_all_kit_burnings:false
+           ~delegate:None
+           ~collateral:(Ligo.tez_from_literal "0mutez")
+           ~adjustment_index:fixedpoint_one
+           ~collateral_at_auction:(Ligo.tez_from_literal "0mutez")
+           ~liquidation_slices:None
+           ~last_touched:(Ligo.timestamp_from_seconds_literal 0) in
+
+       let burrow = Burrow.burrow_burn_kit
+           Parameters.initial_parameters
+           (kit_of_mukit (Ligo.nat_from_literal "1n"))
+           burrow0
+       in
+
+       assert_equal ~printer:show_kit (kit_of_mukit (Ligo.nat_from_literal "1n")) (Burrow.burrow_outstanding_kit burrow);
+       assert_equal ~printer:show_kit kit_zero (Burrow.burrow_excess_kit burrow)
+    );
+
+    ("burrow_burn_kit - burning zero kit returns expected burrow state" >::
+     fun _ ->
+       let burrow0 = Burrow.make_burrow_for_test
+           ~outstanding_kit:(kit_of_mukit (Ligo.nat_from_literal "1n"))
+           ~excess_kit:kit_zero
+           ~active:true
+           ~permission_version:(Ligo.nat_from_literal "0n")
+           ~allow_all_tez_deposits:false
+           ~allow_all_kit_burnings:false
+           ~delegate:None
+           ~collateral:(Ligo.tez_from_literal "0mutez")
+           ~adjustment_index:fixedpoint_one
+           ~collateral_at_auction:(Ligo.tez_from_literal "0mutez")
+           ~liquidation_slices:None
+           ~last_touched:(Ligo.timestamp_from_seconds_literal 0) in
+
+       let burrow = Burrow.burrow_burn_kit
+           Parameters.initial_parameters
+           kit_zero
+           burrow0
+       in
+
+       assert_equal ~printer:show_kit (kit_of_mukit (Ligo.nat_from_literal "1n")) (Burrow.burrow_outstanding_kit burrow);
+       assert_equal ~printer:show_kit kit_zero (Burrow.burrow_excess_kit burrow)
+    );
 
   ]
