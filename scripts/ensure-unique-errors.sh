f034a15983fe3e06cb3b8897cd3a0b335430ef7b
0xd34df00d <0xd34df00d@gmail.com>
2021-03-25 16:55:26 -0500

    Print what error code(s) occur more than once

--- scripts/ensure-unique-errors.sh
+++ scripts/ensure-unique-errors.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 # This awk incantation exists with code 42 if there are duplicates, and 0 otherwise
-grep error_ src/error.ml | awk 'a[$7]++{exit 42}'
+grep error_ src/error.ml | awk 'a[$7]++{print $7; exit 42}'
 
 ret=$?
 
