246410f75d594d6f9dc530f14c17ef77b1a3bb39
0xd34df00d <0xd34df00d@gmail.com>
2021-04-13 18:28:35 +0200

    Add a script for setting up the test keys

---
+++ scripts/setup-keys.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+tezos-client import secret key alice unencrypted:edsk3QoqBuvdamxouPhin7swCvkQNgq4jP5KZPbwWNnwdZpSpJiEbq --force
+tezos-client import secret key bob unencrypted:edsk3RFfvaFaxbHx8BMtEW1rKQcPtDML3LXjNqMNLCzC3wLC1bWbAt --force
