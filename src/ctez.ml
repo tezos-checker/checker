f0e7f2f93469b64810ef47fc3fa9e40086c41ab4
Georgios Karachalias <gdkaracha@gmail.com>
2021-04-20 14:16:35 +0200

    Remove unused parts of ctez.ml

--- src/ctez.ml
+++ src/ctez.ml
@@ -1,14 +1,8 @@
 open Common
-(*
-open FixedPoint
-*)
 
 type ctez = Ligo.nat
 
 let[@inline] ctez_scaling_factor_int = Ligo.int_from_literal "1_000_000"
-(*
-let[@inline] ctez_scaling_factor_nat = Ligo.nat_from_literal "1_000_000n"
-*)
 
 (* Basic arithmetic operations. *)
 let[@inline] ctez_add (x: ctez) (y: ctez) = Ligo.add_nat_nat x y
@@ -17,15 +11,7 @@ let ctez_sub (x: ctez) (y: ctez) =
   | Some n -> n
   | None -> (failwith "Ctez.ctez_sub: negative" : ctez)
 
-(*
-let[@inline] ctez_min (x: ctez) (y: ctez) = if Ligo.leq_nat_nat x y then x else y
-let[@inline] ctez_max (x: ctez) (y: ctez) = if Ligo.geq_nat_nat x y then x else y
-*)
-
 let[@inline] ctez_zero = Ligo.nat_from_literal "0n"
-(*
-let[@inline] ctez_one = ctez_scaling_factor_nat
-*)
 
 (* Conversions to/from other types. *)
 let[@inline] ctez_of_muctez (amnt: Ligo.nat) : ctez = amnt
@@ -44,26 +30,15 @@ let ctez_of_fraction_floor (x_num: Ligo.int) (x_den: Ligo.int) : ctez =
   then (failwith "Ctez.ctez_of_fraction_floor: negative" : ctez)
   else Ligo.abs (fdiv_int_int (Ligo.mul_int_int x_num ctez_scaling_factor_int) x_den)
 
-(*
-let[@inline] ctez_scale (amnt: ctez) (fp: fixedpoint) =
-  ctez_of_fraction_floor
-    (Ligo.mul_int_int (fixedpoint_to_raw fp) (Ligo.int amnt))
-    (Ligo.mul_int_int fixedpoint_scaling_factor ctez_scaling_factor_int)
-*)
-
 (* BEGIN_OCAML *)
 open Ratio
-let[@inline] ctez_to_ratio (amnt: ctez) : ratio = make_real_unsafe (Ligo.int amnt) ctez_scaling_factor_int
+let ctez_to_ratio (amnt: ctez) : ratio = make_real_unsafe (Ligo.int amnt) ctez_scaling_factor_int
+let ratio_of_ctez (x: ctez) : ratio = { num = ctez_to_muctez_int x; den = ctez_scaling_factor_int; }
+
+let ctez_from_tez (tez: Ligo.tez) : ctez = Ligo.abs (tez_to_mutez tez)
 
 let ctez_compare x y = compare_nat x y
 
 let show_ctez amnt = Ligo.string_of_nat amnt ^ "muctez"
 let pp_ctez ppf amnt = Format.fprintf ppf "%s" (show_ctez amnt)
 (* END_OCAML *)
-
-(* FIXME: This is to be removed. *)
-let ctez_from_tez (tez: Ligo.tez) : ctez = Ligo.abs (tez_to_mutez tez)
-(* FIXME: This is to be removed. *)
-let tez_from_ctez (ctez: ctez) : Ligo.tez = Ligo.mul_nat_tez ctez (Ligo.tez_from_literal "1mutez")
-
-let[@inline] ratio_of_ctez (x: ctez) : ratio = { num = ctez_to_muctez_int x; den = ctez_scaling_factor_int; }
--- src/ctez.mli
+++ src/ctez.mli
@@ -1,25 +1,13 @@
 open Ratio
-(*
-open FixedPoint
-*)
 
 type ctez
 
 (* Basic operations. *)
 val ctez_add : ctez -> ctez -> ctez
 val ctez_sub : ctez -> ctez -> ctez
-(*
-val ctez_scale : ctez -> fixedpoint -> ctez
-
-val ctez_min : ctez -> ctez -> ctez
-val ctez_max : ctez -> ctez -> ctez
-*)
 
 val ctez_zero : ctez
 val ctez_scaling_factor_int : Ligo.int
-(*
-val ctez_one : ctez
-*)
 
 (* Conversions to/from other types. *)
 val ctez_of_muctez : Ligo.nat -> ctez
@@ -30,14 +18,11 @@ val ctez_of_fraction_ceil : Ligo.int -> Ligo.int -> ctez
 val ctez_of_fraction_floor : Ligo.int -> Ligo.int -> ctez
 
 (* BEGIN_OCAML *)
-val ctez_compare : ctez -> ctez -> int
-
 val ctez_to_ratio : ctez -> Ratio.ratio
+val ratio_of_ctez: ctez -> ratio
+val ctez_from_tez : Ligo.tez -> ctez
+val ctez_compare : ctez -> ctez -> int
 
 val pp_ctez : Format.formatter -> ctez -> unit
 val show_ctez : ctez -> string
 (* END_OCAML *)
-
-val ctez_from_tez : Ligo.tez -> ctez
-val tez_from_ctez : ctez -> Ligo.tez
-val ratio_of_ctez: ctez -> ratio
