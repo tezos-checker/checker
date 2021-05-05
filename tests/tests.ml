af399319520841373b9f7fa7d103f8366a8f20d4
dorranh <dorran.howell@tweag.io>
2021-05-05 16:49:31 +0200

    Re-enable other tests

--- tests/tests.ml
+++ tests/tests.ml
@@ -2,17 +2,17 @@ open OUnit2
 
 let suite =
   "HuxianTests" >::: [
-    (* TestChecker.suite;
-       TestFixedPoint.suite;
-       TestTez.suite;
-       TestKit.suite;
-       TestAvl.suite;
-       TestBurrow.suite;
-       TestParameters.suite;
-       TestLiquidation.suite;
-       TestCfmm.suite; *)
+    TestChecker.suite;
+    TestFixedPoint.suite;
+    TestTez.suite;
+    TestKit.suite;
+    TestAvl.suite;
+    TestBurrow.suite;
+    TestParameters.suite;
+    TestLiquidation.suite;
+    TestCfmm.suite;
     TestLiquidationAuction.suite;
-    (* TestAvlModel.suite; *)
+    TestAvlModel.suite;
   ]
 
 let () =
