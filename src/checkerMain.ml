eb61cbd1b9f2b477d3ae32f94650cb43a38bc2df
Utku Demir <me@utdemir.com>
2021-05-06 16:05:07 +1200

    Remove redundant let

--- src/checkerMain.ml
+++ src/checkerMain.ml
@@ -53,37 +53,35 @@ let main (op, state: params * wrapper): LigoOp.operation list * wrapper =
 
   let ops, lazy_functions, deployment_state = match deployment_state with
     | Unsealed deployer ->
-      let ops, lazy_functions, deployment_state =
-        if !Ligo.Tezos.sender = deployer
-        then match op with
-          | DeployFunction p ->
-            let lfi, bs = p in
-            let lazy_functions =
-              match Ligo.Big_map.find_opt lfi lazy_functions with
-              | None -> Ligo.Big_map.add lfi bs lazy_functions
-              | Some prev -> Ligo.Big_map.add lfi (Ligo.Bytes.concat prev bs) lazy_functions in
-            (([]: LigoOp.operation list), lazy_functions, Unsealed deployer)
-          | SealContract (oracle_addr, ctez_addr) ->
-            let external_contracts = { oracle = oracle_addr; ctez = ctez_addr; } in
+      if !Ligo.Tezos.sender = deployer
+      then match op with
+        | DeployFunction p ->
+          let lfi, bs = p in
+          let lazy_functions =
+            match Ligo.Big_map.find_opt lfi lazy_functions with
+            | None -> Ligo.Big_map.add lfi bs lazy_functions
+            | Some prev -> Ligo.Big_map.add lfi (Ligo.Bytes.concat prev bs) lazy_functions in
+          (([]: LigoOp.operation list), lazy_functions, Unsealed deployer)
+        | SealContract (oracle_addr, ctez_addr) ->
+          let external_contracts = { oracle = oracle_addr; ctez = ctez_addr; } in
 
-            (* check if the given oracle and ctez contracts have the entrypoints we need *)
-            let _ = get_transfer_ctez_entrypoint external_contracts in
-            let _ = get_oracle_entrypoint external_contracts in
+          (* check if the given oracle and ctez contracts have the entrypoints we need *)
+          let _ = get_transfer_ctez_entrypoint external_contracts in
+          let _ = get_oracle_entrypoint external_contracts in
 
-            (* emit a touch operation to checker *)
-            let touchOp =
-               match (LigoOp.Tezos.get_entrypoint_opt "%touch" !Ligo.Tezos.self_address: unit Ligo.contract option) with
-               | Some c -> LigoOp.Tezos.unit_transaction () (Ligo.tez_from_literal "0mutez") c
-               | None -> (failwith "C1" : LigoOp.operation) in
+          (* emit a touch operation to checker *)
+          let touchOp =
+             match (LigoOp.Tezos.get_entrypoint_opt "%touch" !Ligo.Tezos.self_address: unit Ligo.contract option) with
+             | Some c -> LigoOp.Tezos.unit_transaction () (Ligo.tez_from_literal "0mutez") c
+             | None -> (failwith "C1" : LigoOp.operation) in
 
-            (* initialize checker state *)
-            let checker = initial_checker external_contracts in
+          (* initialize checker state *)
+          let checker = initial_checker external_contracts in
 
-            ([touchOp], lazy_functions, Sealed checker)
-          | CheckerEntrypoint _ ->
-            (Ligo.failwith error_ContractNotDeployed: LigoOp.operation list * lazy_function_map * deployment_state)
-        else (Ligo.failwith error_UnauthorisedCaller: LigoOp.operation list * lazy_function_map * deployment_state) in
-      (ops, lazy_functions, deployment_state)
+          ([touchOp], lazy_functions, Sealed checker)
+        | CheckerEntrypoint _ ->
+          (Ligo.failwith error_ContractNotDeployed: LigoOp.operation list * lazy_function_map * deployment_state)
+      else (Ligo.failwith error_UnauthorisedCaller: LigoOp.operation list * lazy_function_map * deployment_state)
     | Sealed checker ->
       let ops, checker =
         match op with
