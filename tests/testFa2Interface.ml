5d115daa1498d0c2dc7ca066691a4881497a93ca
Georgios Karachalias <gdkaracha@gmail.com>
2021-06-01 14:21:58 +0200

    Add unit tests for fa2_run_update_operators
    Cases covered:
    * Add_operator succeeds if issued by the owner.
    * Add_operator fails if issued by non-owner.
    * Remove_operator succeeds if issued by the owner.
    * Remove_operator fails if issued by non-owner.
    * Add_operator and Remove_operator add and remove operators as expected.
    * fa2_run_update_operators executes the list of operations from left to right.

--- tests/testFa2Interface.ml
+++ tests/testFa2Interface.ml
@@ -7,7 +7,161 @@ type just_boolean = bool
 
 let suite =
   "Fa2Interface tests" >::: [
-    (* fa2_run_transfer tests *)
+    (* ************************************************************************* *)
+    (**                    fa2_run_update_operators tests                        *)
+    (* ************************************************************************* *)
+    ("fa2_run_update_operators - Add_operator - succeeds from owner" >::
+     fun _ ->
+       Ligo.Tezos.reset ();
+       Ligo.Tezos.new_transaction ~seconds_passed:0 ~blocks_passed:0 ~sender:alice_addr ~amount:(Ligo.tez_from_literal "0mutez");
+
+       let fa2_state = initial_fa2_state in
+       let update_op =
+         Add_operator {
+           owner = alice_addr;
+           operator = bob_addr;
+           token_id = kit_token_id;
+         } in
+
+       let _fa2_state = fa2_run_update_operators (fa2_state, [update_op]) in
+       ()
+    );
+
+    ("fa2_run_update_operators - Add_operator - fails from non-owner" >::
+     fun _ ->
+       Ligo.Tezos.reset ();
+       Ligo.Tezos.new_transaction ~seconds_passed:0 ~blocks_passed:0 ~sender:alice_addr ~amount:(Ligo.tez_from_literal "0mutez");
+
+       let fa2_state = initial_fa2_state in
+       let update_op =
+         Add_operator {
+           owner = bob_addr;
+           operator = leena_addr;
+           token_id = kit_token_id;
+         } in
+
+       assert_raises
+         (Failure "FA2_NOT_OWNER")
+         (fun () -> fa2_run_update_operators (fa2_state, [update_op]))
+    );
+
+    ("fa2_run_update_operators - Remove_operator - succeeds from owner" >::
+     fun _ ->
+       Ligo.Tezos.reset ();
+       Ligo.Tezos.new_transaction ~seconds_passed:0 ~blocks_passed:0 ~sender:alice_addr ~amount:(Ligo.tez_from_literal "0mutez");
+
+       let fa2_state = initial_fa2_state in
+       let update_op =
+         Remove_operator {
+           owner = alice_addr;
+           operator = bob_addr;
+           token_id = kit_token_id;
+         } in
+
+       let _fa2_state = fa2_run_update_operators (fa2_state, [update_op]) in
+       ()
+    );
+
+    ("fa2_run_update_operators - Remove_operator - fails from non-owner" >::
+     fun _ ->
+       Ligo.Tezos.reset ();
+       Ligo.Tezos.new_transaction ~seconds_passed:0 ~blocks_passed:0 ~sender:alice_addr ~amount:(Ligo.tez_from_literal "0mutez");
+
+       let fa2_state = initial_fa2_state in
+       let update_op =
+         Remove_operator {
+           owner = bob_addr;
+           operator = leena_addr;
+           token_id = kit_token_id;
+         } in
+
+       assert_raises
+         (Failure "FA2_NOT_OWNER")
+         (fun () -> fa2_run_update_operators (fa2_state, [update_op]))
+    );
+
+    ("fa2_run_update_operators - Add/Remove_operator - add/remove the operator as expected" >::
+     fun _ ->
+       Ligo.Tezos.reset ();
+       Ligo.Tezos.new_transaction ~seconds_passed:0 ~blocks_passed:0 ~sender:alice_addr ~amount:(Ligo.tez_from_literal "0mutez");
+
+       let fa2_state = initial_fa2_state in
+       let update_op =
+         Add_operator {
+           owner = alice_addr;
+           operator = bob_addr;
+           token_id = kit_token_id;
+         } in
+
+       assert_bool
+         "Test potency: starting operators"
+         (not (fa2_is_operator (fa2_state, bob_addr, alice_addr, kit_token_id)));
+
+       let fa2_state = fa2_run_update_operators (fa2_state, [update_op]) in
+
+       assert_bool
+         "Add_operator did not add the operator"
+         (fa2_is_operator (fa2_state, bob_addr, alice_addr, kit_token_id));
+
+       let update_op =
+         Remove_operator {
+           owner = alice_addr;
+           operator = bob_addr;
+           token_id = kit_token_id;
+         } in
+
+       let fa2_state = fa2_run_update_operators (fa2_state, [update_op]) in
+
+       assert_bool
+         "Remove_operator did not remove the operator"
+         (not (fa2_is_operator (fa2_state, bob_addr, alice_addr, kit_token_id)));
+
+       ()
+    );
+
+    ("fa2_run_update_operators - list should be executed from left to right" >::
+     fun _ ->
+       Ligo.Tezos.reset ();
+       Ligo.Tezos.new_transaction ~seconds_passed:0 ~blocks_passed:0 ~sender:alice_addr ~amount:(Ligo.tez_from_literal "0mutez");
+
+       let fa2_state = initial_fa2_state in
+       let add_operator_op =
+         Add_operator {
+           owner = alice_addr;
+           operator = bob_addr;
+           token_id = kit_token_id;
+         } in
+       let remove_operator_op =
+         Remove_operator {
+           owner = alice_addr;
+           operator = bob_addr;
+           token_id = kit_token_id;
+         } in
+
+       assert_bool
+         "Test potency: starting operators"
+         (not (fa2_is_operator (fa2_state, bob_addr, alice_addr, kit_token_id)));
+
+       (* Adding first and removing afterwards should leave bob without privileges. *)
+       let fa2_state = fa2_run_update_operators (fa2_state, [add_operator_op; remove_operator_op;]) in
+
+       assert_bool
+         "Add/Remove should leave no privileges"
+         (not (fa2_is_operator (fa2_state, bob_addr, alice_addr, kit_token_id)));
+
+       (* Removing first and adding afterwards should leave bob with privileges. *)
+       let fa2_state = fa2_run_update_operators (fa2_state, [remove_operator_op; add_operator_op;]) in
+
+       assert_bool
+         "Remove/Add should leave privileges"
+         (fa2_is_operator (fa2_state, bob_addr, alice_addr, kit_token_id));
+
+       ()
+    );
+
+    (* ************************************************************************* *)
+    (**                         fa2_run_transfer tests                           *)
+    (* ************************************************************************* *)
 
     ("fa2_run_transfer - succeeds from owner" >::
      fun _ ->
@@ -52,7 +206,7 @@ let suite =
     ("fa2_run_transfer - succeeds from non-owner but allowed operator" >::
      fun _ ->
        Ligo.Tezos.reset ();
-       Ligo.Tezos.new_transaction ~seconds_passed:0 ~blocks_passed:0 ~sender:alice_addr ~amount:(Ligo.tez_from_literal "0mutez"); (* TODO: try with a different sender; should fail *)
+       Ligo.Tezos.new_transaction ~seconds_passed:0 ~blocks_passed:0 ~sender:alice_addr ~amount:(Ligo.tez_from_literal "0mutez");
 
        let fa2_state = initial_fa2_state in
        let update_op =
