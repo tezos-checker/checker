6a5342a3fa65c06a73081bad755b4729e41850b6
Georgios Karachalias <gdkaracha@gmail.com>
2021-06-01 13:51:26 +0200

    wibbles

--- tests/tests.ml
+++ tests/tests.ml
@@ -2,18 +2,20 @@ open OUnit2
 
 let suite =
   "CheckerTests" >::: [
+    (* fast *)
     TestChecker.suite;
     TestFixedPoint.suite;
     TestTez.suite;
     TestKit.suite;
-    TestAvl.suite;
     TestBurrow.suite;
     TestParameters.suite;
-    TestLiquidation.suite;
     TestCfmm.suite;
+    TestFa2Interface.suite;
+    TestLiquidation.suite;
+    (* slow *)
+    TestAvl.suite;
     TestLiquidationAuction.suite;
     TestAvlModel.suite;
-    TestFa2Interface.suite;
   ]
 
 let () =
