cb126dcf2e570191e747247c2f73f8f9ad9f64ea
Georgios Karachalias <gdkaracha@gmail.com>
2021-06-02 14:18:41 +0200

    More unit tests for fa2_run_transfer

--- tests/testFa2Interface.ml
+++ tests/testFa2Interface.ml
@@ -15,7 +15,7 @@ let mk_kit_tx ~from_ ~to_ ~amount =
     txs =
       [ { to_ = to_;
           token_id = kit_token_id;
-          amount = amount;
+          amount = kit_to_mukit_nat amount;
         }
       ];
   }
@@ -330,7 +330,7 @@ let suite =
        Ligo.Tezos.new_transaction ~seconds_passed:0 ~blocks_passed:0 ~sender:alice_addr ~amount:(Ligo.tez_from_literal "0mutez");
 
        let fa2_state = initial_fa2_state in
-       let tx = mk_kit_tx ~from_:alice_addr ~to_:bob_addr ~amount:(Ligo.nat_from_literal "0n") in
+       let tx = mk_kit_tx ~from_:alice_addr ~to_:bob_addr ~amount:kit_zero in
        let _fa2_state = fa2_run_transfer (fa2_state, [tx]) in
        ()
     );
@@ -341,7 +341,7 @@ let suite =
        Ligo.Tezos.new_transaction ~seconds_passed:0 ~blocks_passed:0 ~sender:bob_addr ~amount:(Ligo.tez_from_literal "0mutez");
 
        let fa2_state = initial_fa2_state in
-       let tx = mk_kit_tx ~from_:alice_addr ~to_:bob_addr ~amount:(Ligo.nat_from_literal "0n") in
+       let tx = mk_kit_tx ~from_:alice_addr ~to_:bob_addr ~amount:kit_zero in
        assert_raises
          (Failure "FA2_NOT_OPERATOR")
          (fun () -> fa2_run_transfer (fa2_state, [tx]))
@@ -358,8 +358,100 @@ let suite =
        let fa2_state = fa2_run_update_operators (fa2_state, [update_op]) in
 
        Ligo.Tezos.new_transaction ~seconds_passed:0 ~blocks_passed:0 ~sender:bob_addr ~amount:(Ligo.tez_from_literal "0mutez");
-       let tx = mk_kit_tx ~from_:alice_addr ~to_:leena_addr ~amount:(Ligo.nat_from_literal "0n") in
+       let tx = mk_kit_tx ~from_:alice_addr ~to_:leena_addr ~amount:kit_zero in
        let _fa2_state = fa2_run_transfer (fa2_state, [tx]) in
+       ()
+    );
+
+    ("fa2_run_transfer - transfers (kit) funds as expected" >::
+     fun _ ->
+       Ligo.Tezos.reset ();
+       let fa2_state_in = initial_fa2_state in
+
+       (* All interesting accounts initially empty. *)
+       assert_kit_equal ~expected:kit_zero ~real:(ask_kit_of fa2_state_in leena_addr);
+       assert_kit_equal ~expected:kit_zero ~real:(ask_kit_of fa2_state_in bob_addr);
+       assert_kit_equal ~expected:kit_zero ~real:(ask_kit_of fa2_state_in alice_addr);
+
+       (* Populate all accounts with different amounts. *)
+       let fa2_state_in =
+         let fa2_state_in = ledger_issue_kit (fa2_state_in, leena_addr, kit_of_mukit (Ligo.nat_from_literal "7_000_000n")) in
+         let fa2_state_in = ledger_issue_kit (fa2_state_in, bob_addr,   kit_of_mukit (Ligo.nat_from_literal "8_000_000n")) in
+         let fa2_state_in = ledger_issue_kit (fa2_state_in, alice_addr, kit_of_mukit (Ligo.nat_from_literal "9_000_000n")) in
+         fa2_state_in in
+
+       (* Ensure all accounts have the expected kit in them. *)
+       assert_kit_equal ~expected:(kit_of_mukit (Ligo.nat_from_literal "7_000_000n")) ~real:(ask_kit_of fa2_state_in leena_addr);
+       assert_kit_equal ~expected:(kit_of_mukit (Ligo.nat_from_literal "8_000_000n")) ~real:(ask_kit_of fa2_state_in bob_addr);
+       assert_kit_equal ~expected:(kit_of_mukit (Ligo.nat_from_literal "9_000_000n")) ~real:(ask_kit_of fa2_state_in alice_addr);
+
+       (* Scenario 1: Alice sends funds to leena directly. *)
+       Ligo.Tezos.new_transaction ~seconds_passed:0 ~blocks_passed:0 ~sender:alice_addr ~amount:(Ligo.tez_from_literal "0mutez");
+       let amount = kit_of_mukit (Ligo.nat_from_literal "123_456n") in
+       let tx = mk_kit_tx ~from_:alice_addr ~to_:leena_addr ~amount:amount in
+       let fa2_state_out = fa2_run_transfer (fa2_state_in, [tx]) in
+
+       assert_kit_equal (* leena's amount goes up *)
+         ~expected:(kit_add (ask_kit_of fa2_state_in leena_addr) amount)
+         ~real:(ask_kit_of fa2_state_out leena_addr);
+       assert_kit_equal (* alice's amount goes down *)
+         ~expected:(ask_kit_of fa2_state_in alice_addr)
+         ~real:(kit_add (ask_kit_of fa2_state_out alice_addr) amount);
+       assert_nat_equal (* total amount of kit stays the same *)
+         ~expected:(fa2_get_total_kit_balance fa2_state_in)
+         ~real:(fa2_get_total_kit_balance fa2_state_out);
+
+       (* Scenario 2: Leena authorizes alice to send funds; alice sends funds to bob. *)
+       let fa2_state_in = fa2_state_out in
+       Ligo.Tezos.new_transaction ~seconds_passed:0 ~blocks_passed:0 ~sender:leena_addr ~amount:(Ligo.tez_from_literal "0mutez");
+       let update_op = add_kit_operator ~owner:leena_addr ~operator:alice_addr in
+       let fa2_state_in = fa2_run_update_operators (fa2_state_in, [update_op]) in
+       Ligo.Tezos.new_transaction ~seconds_passed:0 ~blocks_passed:0 ~sender:alice_addr ~amount:(Ligo.tez_from_literal "0mutez");
+       let amount = kit_of_mukit (Ligo.nat_from_literal "1_500_000n") in
+       let tx = mk_kit_tx ~from_:leena_addr ~to_:bob_addr ~amount:amount in
+       let fa2_state_out = fa2_run_transfer (fa2_state_in, [tx]) in
+
+       assert_kit_equal (* leena's amount goes down *)
+         ~expected:(ask_kit_of fa2_state_in leena_addr)
+         ~real:(kit_add (ask_kit_of fa2_state_out leena_addr) amount);
+       assert_kit_equal (* bob's amount goes up *)
+         ~expected:(kit_add (ask_kit_of fa2_state_in bob_addr) amount)
+         ~real:(ask_kit_of fa2_state_out bob_addr);
+       assert_nat_equal (* total amount of kit stays the same *)
+         ~expected:(fa2_get_total_kit_balance fa2_state_in)
+         ~real:(fa2_get_total_kit_balance fa2_state_out);
+
+       (* Scenario 3: Alice is authorized by leena to send funds; alice sends
+        * funds first from leena to alice, and then from alice to bob. If the
+        * order of execution is not correct, the numbers are high enough for
+        * this operation to fail. *)
+       let fa2_state_in = fa2_state_out in
+       Ligo.Tezos.new_transaction ~seconds_passed:0 ~blocks_passed:0 ~sender:alice_addr ~amount:(Ligo.tez_from_literal "0mutez");
+       let amount1 = kit_of_mukit (Ligo.nat_from_literal "5_623_456n") in
+       let tx1 = mk_kit_tx ~from_:leena_addr ~to_:alice_addr ~amount:amount1 in
+       let amount2 = kit_of_mukit (Ligo.nat_from_literal "14_500_000n") in
+       let tx2 = mk_kit_tx ~from_:alice_addr ~to_:bob_addr ~amount:amount2 in
+
+       (* The numbers are too high for the operations to be executed in the wrong order. *)
+       assert_raises
+         (Failure "FA2_INSUFFICIENT_BALANCE")
+         (fun () -> fa2_run_transfer (fa2_state_in, [tx2; tx1]));
+
+       let fa2_state_out = fa2_run_transfer (fa2_state_in, [tx1; tx2]) in
+
+       assert_kit_equal (* leena's amount goes down by amount1 *)
+         ~expected:(ask_kit_of fa2_state_in leena_addr)
+         ~real:(kit_add (ask_kit_of fa2_state_out leena_addr) amount1);
+       assert_kit_equal (* alice's amount goes up by amount1 by down by amount2 *)
+         ~expected:(kit_add (ask_kit_of fa2_state_in alice_addr) amount1)
+         ~real:(kit_add (ask_kit_of fa2_state_out alice_addr) amount2);
+       assert_kit_equal (* bob's amount goes up by amount2 *)
+         ~expected:(kit_add (ask_kit_of fa2_state_in bob_addr) amount2)
+         ~real:(ask_kit_of fa2_state_out bob_addr);
+       assert_nat_equal (* total amount of kit stays the same *)
+         ~expected:(fa2_get_total_kit_balance fa2_state_in)
+         ~real:(fa2_get_total_kit_balance fa2_state_out);
+
        ()
     );
 
@@ -370,7 +462,6 @@ let suite =
 
     (* TODO: test fa2_get_balance *)
     (* TODO: test fa2_run_balance_of *)
-    (* TODO: test fa2_run_transfer *)
 
     (* FIXME: Not sure how to always make sure that fa2_all_tokens lists all
      * valid tokens. Perhaps with an external script, like we do to check that
