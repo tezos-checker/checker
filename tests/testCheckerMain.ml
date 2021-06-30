4aa6cf3f98778ea6ebbeabac8548fc8cfc61b054
Georgios Karachalias <gdkaracha@gmail.com>
2021-06-30 11:36:50 +0200

    Add execution path to SealContract

--- tests/testCheckerMain.ml
+++ tests/testCheckerMain.ml
@@ -7,7 +7,7 @@ let qcheck_to_ounit t = OUnit.ounit2_of_ounit1 @@ QCheck_ounit.to_ounit_test t
 
 let suite =
   "CheckerMainTests" >::: [
-
+    (* initial_wrapper *)
     (
       qcheck_to_ounit
       @@ QCheck.Test.make
@@ -31,6 +31,23 @@ let suite =
        assert_bool
          "initial metadata bigmap must be empty"
          (List.length (Ligo.Big_map.bindings (initial_wrapper bob_addr).metadata) = 0)
+    );
+
+
+    ("SealContract - no deployments" >::
+     fun _ ->
+       let checker_deployer = leena_addr in
+       Ligo.Tezos.reset ();
+       Ligo.Tezos.new_transaction ~seconds_passed:0 ~blocks_passed:0 ~sender:leena_addr ~amount:(Ligo.tez_from_literal "0mutez");
+
+       let wrapper = CheckerMain.initial_wrapper checker_deployer in (* unsealed *)
+       let ctez_addr = Ligo.address_of_string "ctez_addr" in
+       let oracle_addr = Ligo.address_of_string "oracle_addr" in
+       let op = CheckerMain.SealContract (oracle_addr, ctez_addr) in
+       let _ops, _wrapper = CheckerMain.main (op, wrapper) in (* sealed *)
+       assert_bool "yay!" true;
+
+       ()
     );
 
     (* Add tests here *)
