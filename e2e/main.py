05e8128a9d4cc7f04151d9e6d2288e86ab1186f3
dorranh <dorran.howell@tweag.io>
2021-07-13 09:11:45 +0200

    Functioning version of updated e2e LiquidationsStressTest

--- e2e/main.py
+++ e2e/main.py
@@ -40,6 +40,38 @@ def assert_kit_balance(checker: ContractInterface, address: str, expected_kit: i
     assert expected_kit == kit_balance
 
 
+def avl_storage(checker, ptr: int):
+    """Reads an item from checker's AVL backend using its pointer"""
+    return checker.storage["deployment_state"]["sealed"]["liquidation_auctions"][
+        "avl_storage"
+    ]["mem"][ptr]()
+
+
+def auction_avl_leaves(checker, avl_ptr: int) -> List:
+    """Retrieves a list of all leaves in the specified AVL tree"""
+    node = avl_storage(checker, avl_ptr)
+    node_type = list(node.keys())[0]
+
+    if node_type == "root":
+        next_node, _ = node["root"]
+        # Tree is empty
+        if next_node is None:
+            return []
+        else:
+            for ptr, leaf_node in auction_avl_leaves(checker, next_node):
+                yield ptr, leaf_node
+    elif node_type == "leaf":
+        yield avl_ptr, node
+    else:
+        # We are in a branch
+        # Traverse left side
+        for ptr, leaf_node in auction_avl_leaves(checker, node["branch"]["left"]):
+            yield ptr, leaf_node
+        # Then the right side
+        for ptr, leaf_node in auction_avl_leaves(checker, node["branch"]["right"]):
+            yield ptr, leaf_node
+
+
 # class E2ETest(SandboxedTestCase):
 #     def test_e2e(self):
 #         gas_costs = {}
@@ -295,11 +327,44 @@ class LiquidationsStressTest(SandboxedTestCase):
         #
         # This would use the split method of the AVL tree. However, if the entire queue
         # has less tez than the limit, the 'split' method would trivially return the entire
-        # tree without much effort; so here we should ensure that the liquidation queue has
-        # more tez than the limit (FIXME).
+        # tree without much effort. Here we ensure that the liquidation queue has
+        # more tez than the limit.
+        queued_tez = 0
+        for _, leaf in auction_avl_leaves(
+            checker,
+            checker.storage["deployment_state"]["sealed"]["liquidation_auctions"][
+                "queued_slices"
+            ](),
+        ):
+            queued_tez += leaf["leaf"]["value"]["contents"]["tez"]
+        assert (
+            queued_tez > 10_000_000_000
+        ), "queued tez in liquidation auction was not greater than Constants.max_lot_size which is required for this test"
+
         call_endpoint(checker, "touch", None)
 
-        # And we place a bid
+        # Now that there is an auction started in a descending state, we can select a queued slice
+        # and be confident that we can cancel it before it gets added to another auction.
+        # To successfully do this, we also need to either move the index price such that the
+        # cancellation is warranted or deposit extra collateral to the burrow. Here we do the latter.
+        for queued_leaf_ptr, leaf in auction_avl_leaves(
+            checker,
+            checker.storage["deployment_state"]["sealed"]["liquidation_auctions"][
+                "queued_slices"
+            ](),
+        ):
+            # Only retrieve the first queued slice ptr for efficiency
+            break
+        # Deposit a ton of tez to this burrow to ensure that it is no longer liquidatable
+        call_endpoint(
+            checker,
+            "deposit_tez",
+            leaf["leaf"]["value"]["contents"]["burrow"][1],
+            amount=100_000_000_000,
+        )
+        call_endpoint(checker, "cancel_liquidation_slice", queued_leaf_ptr)
+
+        # And we place a bid:
 
         ret = checker.metadata.currentLiquidationAuctionMinimumBid().storage_view()
         auction_id, minimum_bid = ret["contents"], ret["nat_1"]
@@ -307,9 +372,44 @@ class LiquidationsStressTest(SandboxedTestCase):
         # do not know why we get these names. I think there is an underlying pytezos bug
         # that we should reproduce and create a bug upstream.
 
+        # Note the auction ptr for later operations
+        current_auctions_ptr = checker.storage["deployment_state"]["sealed"][
+            "liquidation_auctions"
+        ]["current_auction"]()["contents"]
         call_endpoint(
             checker, "liquidation_auction_place_bid", (auction_id, minimum_bid)
         )
+        level_after_bid = self.client.shell.head.level()
+
+        # Once max(max_bid_interval_in_blocks, max_bid_interval_in_seconds) has elapsed, the liquidation auction we
+        # bid on should be complete. Here we go off of level since the patched version of Checker we expect to use
+        # sets max_bid_interval_in_blocks=1 and max_bid_interval_in_seconds=1.
+        # Sleeping a bit extra to add a bit of a buffer.
+        time.sleep(10)
+
+        call_endpoint(checker, "touch", None)
+        assert (
+            checker.storage["deployment_state"]["sealed"]["liquidation_auctions"][
+                "completed_auctions"
+            ]()
+            is not None
+        )
+
+        # Before we can claim our winning bid, all of the slices in the completed auction must be touched
+        auctioned_slices = []
+        for leaf_ptr, leaf in auction_avl_leaves(checker, current_auctions_ptr):
+            # Double check that we are only working with leaf ptrs
+            assert "leaf" in leaf
+            auctioned_slices.append(leaf_ptr)
+
+        # Note: batching here based on empirical estimates of how many slices we can
+        # touch in one go before hitting the gas limit.
+        for i in range(0, len(auctioned_slices), 10):
+            slices_to_cancel = auctioned_slices[i : i + 10]
+            call_endpoint(checker, "touch_liquidation_slices", slices_to_cancel)
+
+        # With all of the slices touched, we should now be able to claim our hard-earned winnings
+        call_endpoint(checker, "liquidation_auction_claim_win", current_auctions_ptr)
 
 
 if __name__ == "__main__":
