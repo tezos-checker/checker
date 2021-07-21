c9da2e29c9cd10e1005f5900e845e6aa95e22d3f
Georgios Karachalias <gdkaracha@gmail.com>
2021-07-21 14:14:12 +0200

    Raise reported test coverage of src/common.ml from 97.75% to 100.00%.

--- tests/testCommon.ml
+++ tests/testCommon.ml
@@ -80,17 +80,47 @@ let suite =
          ~real:(clamp_int (Ligo.int_from_literal "8") (Ligo.int_from_literal "-5") (Ligo.int_from_literal "7"));
     );
 
+    "max_tez - first greater than the second" >::
+    (fun _ ->
+       assert_tez_equal
+         ~expected:(Ligo.tez_from_literal "9_337_600_103_819mutez")
+         ~real:(max_tez (Ligo.tez_from_literal "9_337_600_103_819mutez") (Ligo.tez_from_literal "9_337_600_103_818mutez")) (* note: randomly chosen *)
+    );
+
+    "max_tez - second greater than the first" >::
+    (fun _ ->
+       assert_tez_equal
+         ~expected:(Ligo.tez_from_literal "9_337_600_103_819mutez")
+         ~real:(max_tez (Ligo.tez_from_literal "9_337_600_103_818mutez") (Ligo.tez_from_literal "9_337_600_103_819mutez")) (* note: randomly chosen *)
+    );
+
     "fraction_to_tez_floor" >::
     (fun _ ->
        assert_tez_equal
          ~expected:(Ligo.tez_from_literal "333_333mutez")
          ~real:(fraction_to_tez_floor (Ligo.int_from_literal "1") (Ligo.int_from_literal "3"))
     );
+
     "fraction_to_tez_floor - fails for negative numerators" >::
     (fun _ ->
        assert_raises
          (Failure (Ligo.string_of_int internalError_FractionToTezFloorNegative))
          (fun _ -> fraction_to_tez_floor (Ligo.int_from_literal "-1") (Ligo.int_from_literal "2")
+         )
+    );
+
+    "fraction_to_nat_floor" >::
+    (fun _ ->
+       assert_nat_equal
+         ~expected:(Ligo.nat_from_literal "333_333n")
+         ~real:(fraction_to_nat_floor (Ligo.int_from_literal "1_000_000") (Ligo.int_from_literal "3"))
+    );
+
+    "fraction_to_nat_floor - fails for negative numerators" >::
+    (fun _ ->
+       assert_raises
+         (Failure (Ligo.string_of_int internalError_FractionToNatFloorNegative))
+         (fun _ -> fraction_to_nat_floor (Ligo.int_from_literal "-1") (Ligo.int_from_literal "2")
          )
     );
   ]
