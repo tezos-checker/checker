410f5959f607ab0e1778b9e2e481fa1565c64a3a
Georgios Karachalias <gdkaracha@gmail.com>
2021-07-27 16:10:04 +0200

    [wip] store the test coverage results as a json file

---
+++ scripts/coverage-to-json.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import fileinput
+import json
+
+def remove_prefix_if_possible(text, prefix):
+  if text.startswith(prefix):
+    return text[len(prefix):]
+  else:
+    return text
+
+if __name__ == "__main__":
+  filedict = {}
+  for line in fileinput.input():
+    line = line.split()
+    percentage, filename = line[0], line[3]
+    if filename.startswith("src/"):
+      filename = remove_prefix_if_possible(filename, "src/")
+    else:
+      filename = "TOTAL"
+    filedict[filename] = percentage
+
+  json_object = json.dumps(filedict, sort_keys=True, indent=4)
+  print (json_object)
