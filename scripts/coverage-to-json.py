5c60e9dc35af00daf817c9884b9e2f04e33e15da
Georgios Karachalias <gdkaracha@gmail.com>
2021-07-27 16:16:26 +0200

    [wip] remove unused function

--- scripts/coverage-to-json.py
+++ scripts/coverage-to-json.py
@@ -3,21 +3,14 @@
 import fileinput
 import json
 
-
-def remove_prefix_if_possible(text, prefix):
-    if text.startswith(prefix):
-        return text[len(prefix) :]
-    else:
-        return text
-
-
 if __name__ == "__main__":
     filedict = {}
     for line in fileinput.input():
         line = line.split()
         percentage, filename = line[0], line[3]
-        if filename.startswith("src/"):
-            filename = remove_prefix_if_possible(filename, "src/")
+        prefix = "src/"
+        if filename.startswith(prefix):
+            filename = filename[len(prefix) :]
         else:
             filename = "TOTAL"
         filedict[filename] = percentage
