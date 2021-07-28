1957824f8b7746dc1dc4a694d96d66aa2bc23903
Georgios Karachalias <gdkaracha@gmail.com>
2021-07-28 09:54:57 +0200

    structure the json

--- scripts/coverage-to-json.py
+++ scripts/coverage-to-json.py
@@ -4,16 +4,17 @@ import fileinput
 import json
 
 if __name__ == "__main__":
-    filedict = {}
+    filedict = {"files": {}}
     for line in fileinput.input():
         line = line.split()
+        # TODO: would have preferred to use re here; more robust.
         percentage, filename = line[0], line[3]
         prefix = "src/"
         if filename.startswith(prefix):
             filename = filename[len(prefix) :]
+            filedict["files"][filename] = percentage
         else:
-            filename = "TOTAL"
-        filedict[filename] = percentage
+            filedict["total"] = percentage
 
     json_object = json.dumps(filedict, sort_keys=True, indent=4)
     print(json_object)
