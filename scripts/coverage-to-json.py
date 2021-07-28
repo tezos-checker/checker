9dce780b1a97e82a537c9b5b3049e8fd533a14bf
Georgios Karachalias <gdkaracha@gmail.com>
2021-07-28 10:14:40 +0200

    Revert "structure the json"
    This reverts commit 69ddafd4117b09e870b0e18a6e2791e89e7664bb.

--- scripts/coverage-to-json.py
+++ scripts/coverage-to-json.py
@@ -4,17 +4,16 @@ import fileinput
 import json
 
 if __name__ == "__main__":
-    filedict = {"files": {}}
+    filedict = {}
     for line in fileinput.input():
         line = line.split()
-        # TODO: would have preferred to use re here; more robust.
         percentage, filename = line[0], line[3]
         prefix = "src/"
         if filename.startswith(prefix):
             filename = filename[len(prefix) :]
-            filedict["files"][filename] = percentage
         else:
-            filedict["total"] = percentage
+            filename = "TOTAL"
+        filedict[filename] = percentage
 
     json_object = json.dumps(filedict, sort_keys=True, indent=4)
     print(json_object)
