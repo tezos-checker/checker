49862b256674c878613e4fcf0bf385825b1bfd45
dorranh <dorran.howell@tweag.io>
2021-09-21 12:29:02 +0200

    [ci skip] [WIP] Generate tok.ml at build time

---
+++ scripts/builder/checker_builder/config.py
@@ -0,0 +1,105 @@
+"""
+Configuration file schemas for Checker
+"""
+import logging
+import math
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Optional
+
+import yaml
+from jinja2 import Environment, PackageLoader, select_autoescape
+from jinja2.environment import Template
+from marshmallow import Schema, fields
+from marshmallow.decorators import post_load
+
+logging.basicConfig(level=logging.INFO, format="%(message)s")
+logger = logging.getLogger(__name__)
+
+DEFAULT_CONFIG = Path("checker.yaml")
+
+# TODO: Additional support for setting certain parameters at build time can be implemented
+# using:
+# def commented(comment: str, default=None) -> Field:
+#     """Creates a dataclass field with a comment in its metadata"""
+#     return field(default=default, metadata={"comment": comment})
+# @dataclass
+# class SystemParameters:
+#     """Checker system parameters"""
+
+#     fminting: Decimal = commented(
+#         "Dimensionless. Factor used for setting the minting limit.",
+#         default=(Decimal(21) / Decimal(10)),
+#     )
+
+
+@dataclass
+class CollateralTokenConfig:
+    scaling_factor: int = 1_000_000
+
+    @property
+    def decimal_digits(self) -> int:
+        return math.floor(math.log10(self.scaling_factor))
+
+
+@dataclass
+class CheckerConfig:
+    collateral: CollateralTokenConfig
+
+
+# ================================================================================================
+# Schemas
+# ================================================================================================
+class CollateralTokenConfigSchema(Schema):
+    scaling_factor = fields.Integer(strict=True)
+
+    @post_load
+    def make(self, data, **kwargs):
+        return CollateralTokenConfig(**data)
+
+
+class CheckerConfigSchema(Schema):
+    collateral = fields.Nested(CollateralTokenConfigSchema())
+
+    @post_load
+    def make(self, data, **kwargs):
+        return CheckerConfig(**data)
+
+
+def load_checker_config(path: Optional[Path] = None) -> CheckerConfig:
+    if path is None:
+        path = DEFAULT_CONFIG
+    logger.info(f"Loading config from {path}")
+    with path.open() as f:
+        raw_config = yaml.load(f, Loader=yaml.SafeLoader)
+    return CheckerConfigSchema().load(raw_config)
+
+
+def load_template_env() -> Environment:
+    """Gets the default jinja2 environment"""
+    return Environment(
+        loader=PackageLoader("checker_builder"), autoescape=select_autoescape()
+    )
+
+
+def generate_token_module(template: Template, config: CheckerConfig, src_dir: Path):
+    """Generates a tok.ml module using the provided configuration"""
+    module = src_dir.joinpath("tok.ml")
+    logger.info(
+        f"Rendering src module template '{template.name}' using provided config"
+    )
+    rendered = template.render(config=config)
+    logger.info(f"Writing rendered module at {module}")
+    with module.open("w") as f:
+        f.write(rendered)
+
+
+# FIXME: Move this into a CLI
+if __name__ == "__main__":
+    env = load_template_env()
+    template = env.get_template("tok.ml.jinja")
+    config = load_checker_config()
+
+    generate_token_module(
+        template, config, Path("/home/dorran/Documents/nomadic/checker/src")
+    )
