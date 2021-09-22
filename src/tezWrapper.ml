0473d558ae56c03c44f0b2de3916d299cf3eec8b
Georgios Karachalias <gdkaracha@gmail.com>
2021-09-22 10:59:45 +0200

    [skip ci] issue/withdraw

--- src/tezWrapper.ml
+++ src/tezWrapper.ml
@@ -8,8 +8,29 @@ open Common
  * were no vault.
 *)
 
+(* TODO: For the FA2 entrypoints we internally check that the tokens transfered
+ * are valid. We have to include tez_tokens in the list or deal with this in a
+ * different way. I think the safest and easiest way is to use "2n" for tez
+ * tokens and included them in the list. This would implicitly make
+ * fa2_run_transfer behave correctly too (since it depends on
+ * ensure_valid_fa2_token internally). Also we'll need FA2 metadata for
+ * tez_tokens.
+*)
+
 (* TODO: Do we want this to become "2n" and join the other two in fa2Interface.ml? *)
 let[@inline] tez_token_id = Ligo.nat_from_literal "0n"
+
+(* TODO: Move to common.ml. Use in tez_to_mutez as well. *)
+let[@inline] tez_to_mutez_nat (amnt: Ligo.tez) : Ligo.nat =
+  Ligo.div_tez_tez amnt (Ligo.tez_from_literal "1mutez")
+
+let[@inline] ledger_issue_tez_token
+    (st, addr, amnt: fa2_state * Ligo.address * Ligo.tez) : fa2_state =
+  ledger_issue (st, tez_token_id, addr, tez_to_mutez_nat amnt)
+
+let[@inline] ledger_withdraw_tez_token
+    (st, addr, amnt: fa2_state * Ligo.address * Ligo.tez) : fa2_state =
+  ledger_withdraw (st, kit_token_id, addr, tez_to_mutez_nat amnt)
 
 type vault_map = (Ligo.address, Ligo.address) Ligo.big_map
 
@@ -25,7 +46,7 @@ type tez_wrapper_params =
   | Update_operators of fa2_update_operator list
   (* Wrapper-specific entrypoints *)
   | Deposit of unit (* TODO: not nice, having a unit type. Perhaps pass the tez as a number too? *)
-  | Withdraw of Ligo.tez
+  | Withdraw of Ligo.tez (* TODO: the docs said nat, but I think it should be tez *)
   | Set_delegate of (Ligo.key_hash option)
 
 (** Find the address of the vault of given user, or fail. *)
@@ -59,7 +80,7 @@ let wrapper_main (op, state: tez_wrapper_params * tez_wrapper_state): LigoOp.ope
   (* Wrapper-specific entrypoints *)
   | Deposit () ->
     (* 1. Update the balance on the ledger *)
-    let amnt = Ligo.div_tez_tez !Ligo.Tezos.amount (Ligo.tez_from_literal "1mutez") in
+    let amnt = tez_to_mutez_nat !Ligo.Tezos.amount in
     let state_fa2_state = ledger_issue (state.fa2_state, tez_token_id, !Ligo.Tezos.sender, amnt) in
     let state = { state with fa2_state = state_fa2_state } in
     (* 2. Create a vault, if it does not exist already. *)
@@ -74,6 +95,24 @@ let wrapper_main (op, state: tez_wrapper_params * tez_wrapper_state): LigoOp.ope
     in
     ([op], state)
   | Withdraw _amnt ->
+(*
+    let _ = ensure_no_tez_given () in
+*)
     failwith "not implemented yet"
   | Set_delegate _kho ->
     failwith "not implemented yet"
+
+(*
+Fail if Tezos.amount <> 0
+Fail if amount is greater than the Tezos.sender's available FA2 token balance
+Reduce Tezos.sender's token balance by amount
+Emit the following operations:
+A %send_tez operation to the vault corresponding to Tezos.sender with (Tezos.sender, amount)
+
+
+  let new_state_fa2_state =
+    let state_fa2_state = ledger_withdraw_kit (state_fa2_state, !Ligo.Tezos.self_address, slice_kit) in
+    let state_fa2_state = ledger_issue_kit (state_fa2_state, burrow_owner, excess_kit) in
+    state_fa2_state in
+
+*)
