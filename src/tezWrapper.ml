7da43e9c62e438415560cda42294fbcc9b0be760
Georgios Karachalias <gdkaracha@gmail.com>
2021-09-22 14:04:33 +0200

    Vault origination

--- src/tezWrapper.ml
+++ src/tezWrapper.ml
@@ -7,35 +7,45 @@ open VaultTypes
 (**                             {1 VAULTS}                                   *)
 (*****************************************************************************)
 
-let vault_main (p, storage: vault_parameter * vault_storage): LigoOp.operation list * vault_storage =
-  match p with
-  | Vault_set_delegate kho ->
-    let _ = ensure_no_tez_given () in
-    if !Ligo.Tezos.sender <> storage.owner then
-      failwith "unauthorized" (* TODO: Add new error in error.ml *)
-    else
-      ([LigoOp.Tezos.set_delegate kho], storage)
-  | Vault_receive_tez () ->
-    (* TODO: allowed from everyone? *)
-    ([], storage)
-  | Vault_send_tez tz_recipient ->
-    let _ = ensure_no_tez_given () in
-    if !Ligo.Tezos.sender <> storage.owner then
-      failwith "unauthorized" (* TODO: Add new error in error.ml *)
-    else
-      let tz, recipient = tz_recipient in
-      (* TODO: Consider whether we want to have this behavior conflated into
-       * Vault_send_tez, or if it's better to have two separate entrypoints. *)
-      let op = match (LigoOp.Tezos.get_entrypoint_opt "%vault_receive_tez" recipient : unit Ligo.contract option) with
-        | Some c ->
-          LigoOp.Tezos.unit_transaction () tz c
-        | None ->
-          begin match (LigoOp.Tezos.get_contract_opt recipient : unit Ligo.contract option) with
-            | Some c -> LigoOp.Tezos.unit_transaction () tz c
-            | None -> (failwith "failure" : LigoOp.operation) (* TODO: Add new error in error.ml *)
-          end
-      in
-      ([op], storage)
+(* NOTE: We always start with no delegate and no tez. Why? So that we can fire
+ * up vaults on the fly. The only thing we need is the address of the "owner".
+*)
+
+let[@inline] originate_vault (owner: Ligo.address) : LigoOp.operation * Ligo.address =
+  LigoOp.Tezos.vault_create_contract
+    (fun (p, storage : vault_parameter * vault_storage) ->
+       match p with
+       | Vault_set_delegate kho ->
+         let _ = ensure_no_tez_given () in
+         if !Ligo.Tezos.sender <> storage.owner then
+           failwith "unauthorized" (* TODO: Add new error in error.ml *)
+         else
+           ([LigoOp.Tezos.set_delegate kho], storage)
+       | Vault_receive_tez () ->
+         (* TODO: allowed from everyone? *)
+         ([], storage)
+       | Vault_send_tez tz_recipient ->
+         let _ = ensure_no_tez_given () in
+         if !Ligo.Tezos.sender <> storage.owner then
+           failwith "unauthorized" (* TODO: Add new error in error.ml *)
+         else
+           let tz, recipient = tz_recipient in
+           (* TODO: Consider whether we want to have this behavior conflated into
+            * Vault_send_tez, or if it's better to have two separate entrypoints. *)
+           let op = match (LigoOp.Tezos.get_entrypoint_opt "%vault_receive_tez" recipient : unit Ligo.contract option) with
+             | Some c ->
+               LigoOp.Tezos.unit_transaction () tz c
+             | None ->
+               begin match (LigoOp.Tezos.get_contract_opt recipient : unit Ligo.contract option) with
+                 | Some c -> LigoOp.Tezos.unit_transaction () tz c
+                 | None -> (failwith "failure" : LigoOp.operation) (* TODO: Add new error in error.ml *)
+               end
+           in
+           ([op], storage)
+    )
+    (None: Ligo.key_hash option)
+    (Ligo.tez_from_literal "0mutea")
+    {owner = owner}
 
 (*****************************************************************************)
 (**                          {1 WRAPPER TYPES}                               *)
