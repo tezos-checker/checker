c896a9db6880955ee9f0a8a023c125866acaf4f9
Georgios Karachalias <gdkaracha@gmail.com>
2021-10-22 14:48:48 +0200

    Investigate gas costs

--- src/price.ml
+++ src/price.ml
@@ -1,9 +1,9 @@
 open Cfmm
 open CfmmTypes
-open CheckerTypes
+(* open CheckerTypes *)
 open Common
 
-let[@inline] calculate_kit_in_tez (state_cfmm: cfmm) (_state_external_contracts: external_contracts) : ratio =
+let[@inline] calculate_kit_in_tez (state_cfmm: cfmm) (* (_state_external_contracts: external_contracts) *) : ratio =
   (* 1. Get the price of kit in ctez from the cfmm. To avoid having cfmm users
    *    trying to manipulate the price, we use the last price of kit in ctez
    *    observed, not the one in the current block. *)
