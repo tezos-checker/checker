c3a92857ffdb86048302ace91b4cd5650890b538
Georgios Karachalias <gdkaracha@gmail.com>
2021-11-26 16:21:50 +0100

    [wip] [skip ci]

--- client/checker_client/checker.py
+++ client/checker_client/checker.py
@@ -333,6 +333,18 @@ def deploy_wtez(
 ):
     print("Deploying the wtez contract.")
     src = repo.wtez_contract
+
+    # Compute the TZIP-16 metadata
+    token_metadata_view = wtez_token_metadata_view_from_config(config=config)
+    metadata = tzip16_metadata_from_views(functions["views"] + [token_metadata_view])
+    metadata_ser = json.dumps(metadata).encode("utf-8")
+    # FIXME: We also need specific offline views here
+    # FIXME: We also need the "m" entry; re-read TZIP16
+    #   (* add the metadata boilerplate *)
+    #   (* Python: b"tezos-storage:m".hex() *)
+    #   let metadata_url = Ligo.bytes_from_literal "0x74657a6f732d73746f726167653a6d" in
+    #   let metadata = Ligo.Big_map.add "" metadata_url metadata in
+
     initial_storage = {
         "fa2_state": {
             "ledger": {},
@@ -355,6 +367,18 @@ def deploy_wctez(
 ):
     print("Deploying the wctez contract.")
     src = repo.wctez_contract
+
+    # Compute the TZIP-16 metadata
+    token_metadata_view = wctez_token_metadata_view_from_config(config=config)
+    metadata = tzip16_metadata_from_views(functions["views"] + [token_metadata_view])
+    metadata_ser = json.dumps(metadata).encode("utf-8")
+    # FIXME: We also need specific offline views here
+    # FIXME: We also need the "m" entry; re-read TZIP16
+    #   (* add the metadata boilerplate *)
+    #   (* Python: b"tezos-storage:m".hex() *)
+    #   let metadata_url = Ligo.bytes_from_literal "0x74657a6f732d73746f726167653a6d" in
+    #   let metadata = Ligo.Big_map.add "" metadata_url metadata in
+
     initial_storage = {
         "fa2_state": {
             "ledger": {},
@@ -417,10 +441,9 @@ def deploy_checker(
     print("Deploying the TZIP-16 metadata.")
 
     token_metadata_view = checker_token_metadata_view_from_config(config=config)
-
     metadata = tzip16_metadata_from_views(functions["views"] + [token_metadata_view])
-
     metadata_ser = json.dumps(metadata).encode("utf-8")
+
     chunk_size = 10 * 1024
     metadata_chunks = [
         metadata_ser[i : i + chunk_size] for i in range(0, len(metadata_ser), chunk_size)
