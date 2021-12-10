a34fd06d40fadfa629a7fa8ac91f0194e592ae66
Georgios Karachalias <gdkaracha@gmail.com>
2021-12-10 16:42:57 +0100

    Remove the .mli file so that the index-based tests can pass

--- src/oraclePriceType.mli
+++
@@ -1,8 +0,0 @@
-open CheckerTypes
-open FixedPoint
-
-type oracle_price_type
-
-val get_oracle_entrypoint : external_contracts -> (oracle_price_type Ligo.contract) Ligo.contract
-val call_the_oracle : external_contracts -> LigoOp.operation
-val oracle_price_to_fixedpoint : oracle_price_type -> fixedpoint
