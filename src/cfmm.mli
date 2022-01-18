9e221f5e62df2cc31d40d67742be2b42ad1e600a
Georgios Karachalias <gdkaracha@gmail.com>
2022-01-18 16:40:03 +0100

    [wip][skip ci] Cleanup

--- src/cfmm.mli
+++ src/cfmm.mli
@@ -3,55 +3,14 @@ open Ctok
 open Kit
 open Lqt
 open FixedPoint
-(*
-open Constants
-*)
 open CfmmTypes
-(*
-open Error
-*)
 open Common
-
-(*
-(* When the cfmm is uninitialized, we should not be able to query prices
- * and/or do other things. George: I assume that the only thing we should allow
- * is adding liquidity, to kick things off. I would also like to assume that
- * all the conditions below should be either all true or all false, but the
- * implementation of remove_liquidity currently allows liquidity to reach zero.
- * *)
-let[@inline] cfmm_assert_initialized (u: cfmm) : cfmm =
-  assert (not (u.ctok = ctok_zero));
-  assert (not (u.kit = kit_zero));
-  assert (not (u.lqt = lqt_zero));
-  u
-*)
 
 (** Compute the price of kit in ctok (ratio of ctok and kit in the cfmm
     contract), as it was at the end of the last block. This is to be used when
     required for the calculation of the drift derivative instead of up-to-date
     kit_in_ctok, because it is a little harder to manipulate. *)
 val cfmm_kit_in_ctok_in_prev_block : cfmm -> ratio
-
-(*
-(* Update the kit_in_ctok cached and last_level, if we just entered a new block.
- * This should be called before we many any changes to the contract so that we
- * don't lose the last kit_in_ctok at the end of the last block. George: Note
- * that this is not be the previous block, but the last block in which the
- * cfmm contract was touched. *)
-let cfmm_sync_last_observed (cfmm: cfmm) : cfmm =
-  assert (Ligo.geq_nat_nat !Ligo.Tezos.level cfmm.last_level);
-  if Ligo.leq_nat_nat !Ligo.Tezos.level cfmm.last_level then
-    (* do nothing if it's been touched already in this block *)
-    cfmm
-  else
-    { cfmm with
-      kit_in_ctok_in_prev_block =
-        make_ratio
-          (Ligo.mul_nat_int (ctok_to_denomination_nat cfmm.ctok) kit_scaling_factor_int)
-          (Ligo.mul_nat_int (kit_to_denomination_nat cfmm.kit) ctok_scaling_factor_int);
-      last_level = !Ligo.Tezos.level;
-    }
-*)
 
 (** Compute the maximum [min_kit_expected] for [cfmm_buy_kit] to succeed. *)
 val cfmm_view_min_kit_expected_buy_kit : cfmm -> fixedpoint -> ctok -> (kit * cfmm)
